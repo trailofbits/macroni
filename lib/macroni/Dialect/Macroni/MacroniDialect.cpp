58411a7b5fad92c945db17826d0f0b2aaf1daffa
Brent Pappas <pappasbrent@gmail.com>
2023-06-20 08:32:02 -0400

    Register Macroni dialect and operations

---
+++ lib/macroni/Dialect/Macroni/MacroniDialect.cpp
@@ -0,0 +1,31 @@
+// Copyright (c) 2023-present, Trail of Bits, Inc.
+
+#include "macroni/Dialect/Macroni/MacroniDialect.hpp"
+#include "macroni/Dialect/Macroni/MacroniOps.hpp"
+
+#include <mlir/IR/TypeSupport.h>
+#include <mlir/IR/Builders.h>
+#include <mlir/IR/DialectImplementation.h>
+
+#include <llvm/ADT/TypeSwitch.h>
+#include <llvm/Support/ErrorHandling.h>
+#include <llvm/Support/SMLoc.h>
+
+namespace macroni::macroni
+{
+    void MacroniDialect::initialize()
+    {
+        registerTypes();
+        registerAttributes();
+        addOperations<
+            #define GET_OP_LIST
+            #include "macroni/Dialect/Macroni/Macroni.cpp.inc"
+        >();
+    }
+
+    using DialectParser = mlir::AsmParser;
+    using DialectPrinter = mlir::AsmPrinter;
+
+} // namespace macroni::macroni
+
+#include "macroni/Dialect/Macroni/MacroniDialect.cpp.inc"
