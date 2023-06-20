e0c6ef082bf28e3d6d3787ed1c9ea6b9d1480201
Brent Pappas <pappasbrent@gmail.com>
2023-06-20 08:29:41 -0400

    Add Macroni dialect header file

---
+++ include/macroni/Dialect/Macroni/MacroniDialect.hpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2022-present, Trail of Bits, Inc.
+
+#pragma once
+
+#include "vast/Util/Warnings.hpp"
+
+VAST_RELAX_WARNINGS
+#include <mlir/IR/BuiltinOps.h>
+#include <mlir/IR/BuiltinTypes.h>
+#include <mlir/IR/Dialect.h>
+#include <mlir/IR/OperationSupport.h>
+#include <mlir/Interfaces/SideEffectInterfaces.h>
+#include <mlir/Interfaces/ControlFlowInterfaces.h>
+#include <mlir/Support/TypeID.h>
+VAST_RELAX_WARNINGS
+
+// Pull in the dialect definition.
+#include "macroni/Dialect/Macroni/MacroniDialect.h.inc"
