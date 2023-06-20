effd87f969b0ccbc5a335fb35eca1ae77808cc53
Brent Pappas <pappasbrent@gmail.com>
2023-06-20 08:28:08 -0400

    Add files defining Macroni attributes and types
    - Add TableGen and C++ header files defining Macroni attributes and
      types. Currently Macroni does not have any attributes or types, but I
      will leave these files here for now in case Macroni will eventually
      need attributes and types. If not then I will remove these files later.

---
+++ include/macroni/Dialect/Macroni/MacroniAttributes.hpp
@@ -0,0 +1,8 @@
+// Copyright (c) 2023-present, Trail of Bits, Inc.
+
+#pragma once
+
+#include "macroni/Dialect/Macroni/MacroniDialect.hpp"
+
+#define GET_ATTRDEF_CLASSES
+#include "macroni/Dialect/Macroni/MacroniAttributes.h.inc"
