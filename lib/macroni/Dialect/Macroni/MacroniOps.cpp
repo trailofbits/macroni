17b0e6d578b3484db6ab161d2455b8b162ae6688
Brent Pappas <pappasbrent@gmail.com>
2023-06-22 14:29:52 -0400

    Add custom parser and printer for macro expansions
    - Add a custom printer MLIR Macroni macro expansion operations. Prints
      the name of object-like macros, and for function-like macros, prints
      the parameter list as well.
    - Add a custom parser for MLIR Macroni macro expansions. This parses the
      output of the custom printer. This has not yet been tested to work.

--- lib/macroni/Dialect/Macroni/MacroniOps.cpp
+++ lib/macroni/Dialect/Macroni/MacroniOps.cpp
@@ -50,6 +50,100 @@ namespace macroni::macroni {
             expansionBuilder.value()(odsBuilder, odsState.location);
         }
     }
+
+    mlir::ParseResult parseMacroParameters(
+        mlir::OpAsmParser &parser,
+        mlir::StringAttr &macroName,
+        mlir::BoolAttr &functionLike,
+        mlir::ArrayAttr &parameterNames) {
+
+        mlir::Builder &builder = parser.getBuilder();
+
+        std::string string_result;
+        mlir::ParseResult parse_result = parser.parseString(&string_result);
+        if (parse_result.succeeded()) {
+            macroName = builder.getStringAttr(llvm::Twine(string_result));
+        } else {
+            return mlir::failure();
+        }
+
+        // NOTE(bpp): Use optional LParen or mandatory LParen here? Not sure
+        // what the difference is if we check for success either way
+        parse_result = parser.parseOptionalLParen();
+        if (parse_result.succeeded()) {
+            functionLike = builder.getBoolAttr(true);
+            llvm::SmallVector<llvm::StringRef, 4> param_names_vec;
+            parse_result = parser.parseCommaSeparatedList(
+                [&]() {
+                    if (parser.parseString(&string_result).succeeded()) {
+                        auto string_ref = llvm::StringRef(string_result);
+                        param_names_vec.push_back(string_ref);
+                        return mlir::success();
+                    } else {
+                        return mlir::failure();
+                    }
+                });
+            if (parse_result.succeeded()) {
+                auto param_names_array_ref =
+                    llvm::ArrayRef<llvm::StringRef>(param_names_vec);
+                parameterNames = builder.getStrArrayAttr(param_names_array_ref);
+            } else {
+                return mlir::failure();
+            }
+        } else {
+            functionLike = builder.getBoolAttr(false);
+            // TODO(bpp): Check that no parameters are provided?
+        }
+
+        return mlir::success();
+    }
+
+    void printMacroParametersCommon(
+        mlir::OpAsmPrinter &printer,
+        mlir::StringAttr macroName,
+        mlir::BoolAttr functionLike,
+        mlir::ArrayAttr parameterNames) {
+        llvm::raw_ostream &os = printer.getStream();
+
+        os << '"' << macroName.getValue();
+        if (functionLike.getValue()) {
+            int i = 0;
+            os << '(';
+            auto range = parameterNames.getAsValueRange<mlir::StringAttr>();
+            for (const auto &&param_name : range) {
+                if (i > 0) {
+                    os << ", ";
+                }
+                os << param_name;
+                i++;
+            }
+            os << ')';
+        }
+        os << '"';
+    }
+
+    void printMacroParameters(
+        mlir::OpAsmPrinter &printer,
+        MacroExpansionExpr op,
+        mlir::StringAttr macroName,
+        mlir::BoolAttr functionLike,
+        mlir::ArrayAttr parameterNames) {
+        printMacroParametersCommon(printer, macroName,
+                                   functionLike, parameterNames);
+    }
+
+
+
+    void printMacroParameters(
+        mlir::OpAsmPrinter &printer,
+        MacroExpansionStmt op,
+        mlir::StringAttr macroName,
+        mlir::BoolAttr functionLike,
+        mlir::ArrayAttr parameterNames) {
+        printMacroParametersCommon(printer, macroName,
+                                   functionLike, parameterNames);
+    }
+
 }
 
 #define GET_OP_CLASSES
