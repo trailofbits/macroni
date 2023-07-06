2bab44b78c3f2d4eea744b57e88a1fec6f064edf
Brent Pappas <pappasbrent@gmail.com>
2023-07-06 16:41:07 -0400

    Add build method for kernel::ListForEach

---
+++ lib/macroni/Dialect/Kernel/KernelOps.cpp
@@ -0,0 +1,33 @@
+// Copyright (c) 2023-present, Trail of Bits, Inc.
+
+
+#include "macroni/Dialect/Kernel/KernelDialect.hpp"
+#include "macroni/Dialect/Kernel/KernelOps.hpp"
+
+#include <mlir/Support/LLVM.h>
+#include <mlir/Support/LogicalResult.h>
+#include <mlir/IR/Builders.h>
+#include <mlir/IR/OperationSupport.h>
+#include <mlir/IR/SymbolTable.h>
+#include <mlir/IR/OpImplementation.h>
+#include <mlir/IR/FunctionImplementation.h>
+#include <llvm/Support/ErrorHandling.h>
+
+namespace macroni::kernel {
+
+    void ListForEach::build(
+        mlir::OpBuilder &odsBuilder,
+        mlir::OperationState &odsState,
+        mlir::Value pos,
+        mlir::Value head,
+        std::unique_ptr<mlir::Region> &&region
+    ) {
+        mlir::OpBuilder::InsertionGuard guard(odsBuilder);
+        odsState.addOperands(pos);
+        odsState.addOperands(head);
+        odsState.addRegion(std::move(region));
+    }
+}
+
+#define GET_OP_CLASSES
+#include "macroni/Dialect/Kernel/Kernel.cpp.inc"
