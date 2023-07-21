b6e1775040b6e3565ea0b7fe9039b4597e5c1d1b
Brent Pappas <pappasbrent@gmail.com>
2023-07-21 10:33:03 -0400

    Remove global variables header

--- bin/Macronify/Globals.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <mlir/IR/Builders.h>
-#include <optional>
-#include <pasta/AST/AST.h>
-
-// TODO(bpp): Instead of using a global variable for the PASTA AST and MLIR
-// context, find out how to pass these to a CodeGen object.
-extern std::optional<pasta::AST> ast;
