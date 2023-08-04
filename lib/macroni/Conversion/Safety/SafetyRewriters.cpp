2a3fadfe13e48f2839e4c20dc96efca39016a4e9
Brent Pappas <pappasbrent@gmail.com>
2023-08-04 10:49:23 -0400

    Add early return to `rewrite_unsafe()`

--- lib/macroni/Conversion/Safety/SafetyRewriters.cpp
+++ lib/macroni/Conversion/Safety/SafetyRewriters.cpp
@@ -5,14 +5,14 @@ namespace macroni::safety {
     mlir::LogicalResult rewrite_unsafe(
         vast::hl::IfOp if_op,
         mlir::PatternRewriter &rewriter) {
-        if (if_op->hasAttr("unsafe")) {
-            auto unsafe_op = rewriter.create<UnsafeRegion>(if_op.getLoc());
-            auto &reg = unsafe_op.getBodyRegion();
-            rewriter.inlineRegionBefore(if_op.getThenRegion(), reg, reg.end());
-            rewriter.eraseOp(if_op);
-            return mlir::success();
+        if (!if_op->hasAttr("unsafe")) {
+            return mlir::failure();
         }
-        return mlir::failure();
+        auto unsafe_op = rewriter.create<UnsafeRegion>(if_op.getLoc());
+        auto &reg = unsafe_op.getBodyRegion();
+        rewriter.inlineRegionBefore(if_op.getThenRegion(), reg, reg.end());
+        rewriter.eraseOp(if_op);
+        return mlir::success();
     }
 
 } // namespace macroni::safety
