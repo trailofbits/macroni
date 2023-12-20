4208a191cd0e89cc4923627fa9a9d035941bf7bd
Brent Pappas <pappasbrent@gmail.com>
2023-12-20 15:49:19 -0500

    Move includes to source file

--- include/macroni/Common/ParseAST.hpp
+++ include/macroni/Common/ParseAST.hpp
@@ -1,11 +1,6 @@
 #pragma once
 
 #include <pasta/AST/AST.h>
-#include <pasta/Compile/Command.h>
-#include <pasta/Compile/Compiler.h>
-#include <pasta/Util/ArgumentVector.h>
-#include <pasta/Util/FileSystem.h>
-#include <pasta/Util/Init.h>
 
 namespace pasta {
 Result<AST, std::string> parse_ast(int argc, char **argv);
--- lib/macroni/Common/ParseAST.cpp
+++ lib/macroni/Common/ParseAST.cpp
@@ -1,4 +1,9 @@
 #include <macroni/Common/ParseAST.hpp>
+#include <pasta/Compile/Command.h>
+#include <pasta/Compile/Compiler.h>
+#include <pasta/Util/ArgumentVector.h>
+#include <pasta/Util/FileSystem.h>
+#include <pasta/Util/Init.h>
 
 namespace pasta {
 Result<AST, std::string> parse_ast(int argc, char **argv) {
