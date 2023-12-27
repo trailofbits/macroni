17061d2419f010f4ddf1974e1daf5e1d1ec796d5
Brent Pappas <pappasbrent@gmail.com>
2023-12-27 12:37:11 -0500

    Add draft code for checking for RCU warnings

--- bin/Kernelize/Main.cpp
+++ bin/Kernelize/Main.cpp
@@ -9,6 +9,7 @@
 #include <macroni/Common/GenerateMacroniModule.hpp>
 #include <macroni/Common/ParseAST.hpp>
 #include <macroni/Conversion/Kernel/KernelRewriters.hpp>
+#include <mlir/IR/Diagnostics.h>
 #include <mlir/Pass/Pass.h>
 #include <mlir/Pass/PassManager.h>
 #include <mlir/Transforms/GreedyPatternRewriteDriver.h>
@@ -27,7 +28,8 @@ int main(int argc, char **argv) {
 
   // Register the MLIR dialects we will be lowering to
   mlir::DialectRegistry registry;
-  registry.insert<vast::hl::HighLevelDialect, macroni::macroni::MacroniDialect,
+  registry.insert<vast::hl::HighLevelDialect, vast::unsup::UnsupportedDialect,
+                  macroni::macroni::MacroniDialect,
                   macroni::kernel::KernelDialect>();
   auto mctx = mlir::MLIRContext(registry);
 
@@ -40,21 +42,77 @@ int main(int argc, char **argv) {
       .add(macroni::kernel::rewrite_offsetof)
       .add(macroni::kernel::rewrite_container_of)
       .add(macroni::kernel::rewrite_rcu_dereference)
+      .add(macroni::kernel::rewrite_rcu_dereference_check)
       .add(macroni::kernel::rewrite_smp_mb)
       .add(macroni::kernel::rewrite_list_for_each)
+      .add(macroni::kernel::rewrite_label_stmt)
       .add(macroni::kernel::rewrite_rcu_read_unlock);
 
   // Apply the conversions
   auto frozen_pats = mlir::FrozenRewritePatternSet(std::move(patterns));
   mod->walk([&frozen_pats](mlir::Operation *op) {
     if (mlir::isa<macroni::macroni::MacroExpansion, vast::hl::ForOp,
-                  vast::hl::CallOp>(op)) {
+                  vast::hl::CallOp, vast::hl::LabelStmt>(op)) {
       std::ignore = mlir::applyOpPatternsAndFold(op, frozen_pats);
     }
   });
 
   // Print the result
   mod->print(llvm::outs());
+
+  mlir::DiagnosticEngine &engine = mctx.getDiagEngine();
+  auto diagnostic_handler = engine.registerHandler([](mlir::Diagnostic &diag) {
+    diag.print(llvm::errs());
+    return;
+  });
+
+  mod->walk<mlir::WalkOrder::PreOrder>([](mlir::Operation *op) {
+    if (mlir::isa<macroni::kernel::RCUCriticalSection>(op)) {
+      // NOTE(bpp): Skip checking for invocations of RCU macros inside RCU
+      // critical sections because we only want to emit warnings for invocations
+      // of RCU macros outside of critical sections. We walk the tree using
+      // pre-order traversal instead of using post-order traversal (the default)
+      // in order for this to work.
+      return mlir::WalkResult::skip();
+    }
+    if (mlir::isa<macroni::kernel::RCUDereference,
+                  macroni::kernel::RCUDereferenceBH,
+                  macroni::kernel::RCUDereferenceSched,
+                  macroni::kernel::RCUDereferenceCheck,
+                  macroni::kernel::RCUDereferenceBHCheck,
+                  macroni::kernel::RCUDereferenceSchedCheck,
+                  macroni::kernel::RCUDereferenceProtected>(op)) {
+      auto name = "<error>";
+      if (mlir::isa<macroni::kernel::RCUDereference>(op)) {
+        name = "rcu_dereference";
+      } else if (mlir::isa<macroni::kernel::RCUDereferenceBH>(op)) {
+        name = "rcu_dereference_bh";
+      } else if (mlir::isa<macroni::kernel::RCUDereferenceSched>(op)) {
+        name = "rcu_dereference_sched";
+      } else if (mlir::isa<macroni::kernel::RCUDereferenceCheck>(op)) {
+        name = "rcu_dereference_check";
+      } else if (mlir::isa<macroni::kernel::RCUDereferenceBHCheck>(op)) {
+        name = "rcu_dereference_bh_check";
+      } else if (mlir::isa<macroni::kernel::RCUDereferenceSchedCheck>(op)) {
+        name = "rcu_dereference_sched_check";
+      } else if (mlir::isa<macroni::kernel::RCUDereferenceProtected>(op)) {
+        name = "rcu_dereference_protected";
+      }
+      std::string s;
+      auto os = llvm::raw_string_ostream(s);
+      op->getLoc()->print(os);
+      s.erase(s.find("loc("), 4);
+      s.erase(s.find('"'), 1);
+      s.erase(s.find('"'), 1);
+      s.erase(s.rfind(')'), 1);
+      os << ": warning: Invocation of " << name
+         << "() outside of RCU critical section\n";
+      op->emitWarning() << s;
+    }
+    return mlir::WalkResult::advance();
+  });
+
+  engine.eraseHandler(diagnostic_handler);
 
   return EXIT_SUCCESS;
 }
