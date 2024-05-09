29bc07291192c51dfeb0b0437b096e626e47df8c
Brent Pappas <pappasbrent@gmail.com>
2024-05-09 09:24:21 -0400

    Remove setup_package_version_variables() macro
    Remove the setup_package_version_variables() macro since it is no longer
    necessary

--- cmake/project_utils.cmake
+++ cmake/project_utils.cmake
@@ -78,33 +78,3 @@ function(find_and_select_clang_compiler)
     endif()
   endif()
 endfunction()
-
-macro(setup_package_version_variables _packageName)
-  # TODO(Brent): Refactor this macro into a function with arguments and out
-  # variables.
-  list(APPEND CMAKE_MESSAGE_CONTEXT "projectUtils.setupPackageVersionVariables")
-
-  if(DEFINED ${_packageName}_VERSION)
-    string(REGEX MATCHALL "[0-9]+" _versionComponents
-                 "${${_packageName}_VERSION}")
-    list(LENGTH _versionComponents _len)
-    if(${_len} GREATER 0)
-      list(GET _versionComponents 0 ${_packageName}_VERSION_MAJOR)
-    endif()
-    if(${_len} GREATER 1)
-      list(GET _versionComponents 1 ${_packageName}_VERSION_MINOR)
-    endif()
-    if(${_len} GREATER 2)
-      list(GET _versionComponents 2 ${_packageName}_VERSION_PATCH)
-    endif()
-    if(${_len} GREATER 3)
-      list(GET _versionComponents 3 ${_packageName}_VERSION_TWEAK)
-    endif()
-    set(${_packageName}_VERSION_COUNT ${_len})
-  else()
-    set(${_packageName}_VERSION_COUNT 0)
-    set(${_packageName}_VERSION "")
-  endif()
-
-  list(POP_BACK CMAKE_MESSAGE_CONTEXT)
-endmacro()
