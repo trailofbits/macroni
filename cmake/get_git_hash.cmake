486c8baef9137f090652767fce16b3c0f7062aa7
Brent Pappas <pappasbrent@gmail.com>
2024-05-09 11:38:51 -0400

    Reformat with cmake-format

--- cmake/get_git_hash.cmake
+++ cmake/get_git_hash.cmake
@@ -3,10 +3,11 @@ function(get_git_hash function_output_variable)
     COMMAND ${GIT_EXECUTABLE} rev-parse HEAD
     RESULT_VARIABLE result
     OUTPUT_VARIABLE git_hash
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-  )
-  if (result)
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+  if(result)
     message(FATAL_ERROR "Failed to get git hash: ${result}")
   endif()
-  set(${function_output_variable} ${git_hash} PARENT_SCOPE)
+  set(${function_output_variable}
+      ${git_hash}
+      PARENT_SCOPE)
 endfunction()
