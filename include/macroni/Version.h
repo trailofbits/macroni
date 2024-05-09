9db533c0744e84f8a73222bd7dd17266c5d25434
Brent Pappas <pappasbrent@gmail.com>
2024-05-09 10:42:38 -0400

    Delete Version.h since Config.h replaces it

--- include/macroni/Version.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2024-present, Trail of Bits, Inc.
-
-#pragma once
-
-extern const char *macroni_version;
-extern unsigned macroni_version_major;
-extern unsigned macroni_version_minor;
-extern unsigned macroni_version_patch;
-extern unsigned macroni_version_tweak;
