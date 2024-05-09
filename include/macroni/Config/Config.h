d5dc593d014f95b8a8aad150b2b081291ff3d14d
Brent Pappas <pappasbrent@gmail.com>
2024-05-09 10:41:32 -0400

    Add version info to Config.h

---
+++ include/macroni/Config/Config.h
@@ -0,0 +1,21 @@
+//
+// Copyright (c) 2023-present, Trail of Bits, Inc.
+// All rights reserved.
+//
+// This source code is licensed in accordance with the terms specified in
+// the LICENSE file found in the root directory of this source tree.
+//
+
+/* This generated file is for internal use. Do not include it from headers. */
+
+#pragma once
+
+namespace macroni {
+extern const char *macroni_version;
+extern unsigned macroni_version_major;
+extern unsigned macroni_version_minor;
+extern unsigned macroni_version_patch;
+extern unsigned macroni_version_tweak;
+
+extern const char *bug_report_url;
+} // namespace macroni
