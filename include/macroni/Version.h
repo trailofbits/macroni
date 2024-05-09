a734d2470ae3e768b1983d8c9671a029a63ec8eb
Brent Pappas <pappasbrent@gmail.com>
2024-05-09 10:12:18 -0400

    Configure macroni version info in a source file
    Configure macroni version info in a source file and not a header file so
    that any file references Macroni's version number, they won't need to be
    recompiled, just relinked.

---
+++ include/macroni/Version.h
@@ -0,0 +1,9 @@
+// Copyright (c) 2024-present, Trail of Bits, Inc.
+
+#pragma once
+
+extern const char *macroni_version;
+extern unsigned macroni_version_major;
+extern unsigned macroni_version_minor;
+extern unsigned macroni_version_patch;
+extern unsigned macroni_version_tweak;
