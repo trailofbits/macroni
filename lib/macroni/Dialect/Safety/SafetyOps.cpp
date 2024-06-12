572007a35fc730cfbf1dd1203fc18710bac60153
Brent Pappas <pappasbrent@gmail.com>
2024-06-12 11:49:57 -0400

    Add builder for UnsafeRegion.

--- lib/macroni/Dialect/Safety/SafetyOps.cpp
+++ lib/macroni/Dialect/Safety/SafetyOps.cpp
@@ -2,17 +2,30 @@
 
 #include "macroni/Dialect/Safety/SafetyOps.hpp"
 #include "macroni/Dialect/Safety/SafetyDialect.hpp"
+#include "vast/Util/Common.hpp"
 
 #include <llvm/Support/ErrorHandling.h>
 #include <mlir/IR/Builders.h>
-#include <mlir/IR/FunctionImplementation.h>
 #include <mlir/IR/OpImplementation.h>
 #include <mlir/IR/OperationSupport.h>
 #include <mlir/IR/SymbolTable.h>
 #include <mlir/Support/LLVM.h>
 #include <mlir/Support/LogicalResult.h>
 
-namespace macroni::safety {}
+namespace macroni::safety {
+void build_region(vast::mlir_builder &bld, vast::op_state &st,
+                  vast::builder_callback_ref callback) {
+  bld.createBlock(st.addRegion());
+  callback(bld, st.location);
+}
+
+void UnsafeRegion::build(mlir::OpBuilder &odsBuilder,
+                         mlir::OperationState &odsState,
+                         vast::builder_callback_ref callback) {
+  mlir::OpBuilder::InsertionGuard guard(odsBuilder);
+  build_region(odsBuilder, odsState, callback);
+}
+} // namespace macroni::safety
 
 #define GET_OP_CLASSES
 #include "macroni/Dialect/Safety/Safety.cpp.inc"
