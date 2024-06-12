092f5947531eec0a3c0b6a34819488eec6a7b590
Brent Pappas <pappasbrent@gmail.com>
2024-06-12 16:00:32 -0400

    Add empty visitor
    Add an empty visitor that just returns empty operations so that macroni
    visitors may inherit from it and only override the visitors that they
    are interested in hooking into.

---
+++ include/macroni/Common/EmptyVisitor.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "vast/CodeGen/CodeGenBuilder.hpp"
+#include "vast/CodeGen/CodeGenMeta.hpp"
+#include "vast/CodeGen/CodeGenVisitorBase.hpp"
+#include "vast/CodeGen/SymbolGenerator.hpp"
+#include "vast/Util/Common.hpp"
+
+namespace macroni {
+struct empty_visitor : vast::cg::visitor_base {
+  [[nodiscard]] empty_visitor(vast::mcontext_t &mctx,
+                              vast::cg::meta_generator &mg,
+                              vast::cg::symbol_generator &sg,
+                              vast::cg::visitor_view view);
+
+  vast::operation visit(const vast::cg::clang_stmt *stmt,
+                        vast::cg::scope_context &scope) override;
+
+  vast::operation visit(const vast::cg::clang_decl *decl,
+                        vast::cg::scope_context &scope) override;
+
+  vast::mlir_type visit(const vast::cg::clang_type *type,
+                        vast::cg::scope_context &scope) override;
+
+  vast::mlir_type visit(vast::cg::clang_qual_type type,
+                        vast::cg::scope_context &scope) override;
+
+  vast::mlir_attr visit(const vast::cg::clang_attr *attr,
+                        vast::cg::scope_context &scope) override;
+
+  vast::operation visit_prototype(const vast::cg::clang_function *decl,
+                                  vast::cg::scope_context &scope) override;
+};
+} // namespace macroni
