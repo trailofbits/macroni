54ff825d28d9e3451046ee82a7503f22f120e6e2
Brent Pappas <pappasbrent@gmail.com>
2024-06-14 14:56:30 -0400

    Revise rcu_dereference() and rcu_assign_pointer()
    Revise testsw for rcu_dereference() and rcu_assign_pointer() to use the
    definitions of the macros as they appear in the kernel.

--- test/KernelTests/rcu_assign_pointer.c
+++ test/KernelTests/rcu_assign_pointer.c
@@ -1,47 +1,16 @@
-// RUN: kernelize -x c %s | FileCheck %s --match-full-lines
+#include "rcu.h"
 
-#define rcu_assign_pointer(p, v) ((p) = (v))
+struct string {
+  char *begin;
+  unsigned long size;
+};
 
 int main(void) {
-  int *p, *v;
+  int *p = 0, *v = 0;
+  char *new_begin = 0;
+  struct string *s = {0};
+
   rcu_assign_pointer(p, v);
+  rcu_assign_pointer(s->begin, new_begin);
   return 0;
 }
-
-// CHECK:hl.translation_unit {
-// CHECK:  hl.typedef "__int128_t" : !hl.int128
-// CHECK:  hl.typedef "__uint128_t" : !hl.int128< unsigned >
-// CHECK:  hl.struct "__NSConstantString_tag" {type_visibility = #unsup<attr "type_visibility">} : {
-// CHECK:    hl.field "isa" : !hl.ptr<!hl.int< const >>
-// CHECK:    hl.field "flags" : !hl.int
-// CHECK:    hl.field "str" : !hl.ptr<!hl.char< const >>
-// CHECK:    hl.field "length" : !hl.long
-// CHECK:  }
-// CHECK:  hl.typedef "__NSConstantString" : !hl.record<"__NSConstantString_tag">
-// CHECK:  hl.typedef "__builtin_ms_va_list" : !hl.ptr<!hl.char>
-// CHECK:  hl.struct "__va_list_tag" {type_visibility = #unsup<attr "type_visibility">} : {
-// CHECK:    hl.field "gp_offset" : !hl.int< unsigned >
-// CHECK:    hl.field "fp_offset" : !hl.int< unsigned >
-// CHECK:    hl.field "overflow_arg_area" : !hl.ptr<!hl.void>
-// CHECK:    hl.field "reg_save_area" : !hl.ptr<!hl.void>
-// CHECK:  }
-// CHECK:  hl.typedef "__builtin_va_list" : !hl.array<1, !hl.record<"__va_list_tag">>
-// CHECK:  hl.func @main () -> !hl.int {
-// CHECK:    core.scope {
-// CHECK:      %0 = hl.var "p" : !hl.lvalue<!hl.ptr<!hl.int>>
-// CHECK:      %1 = hl.var "v" : !hl.lvalue<!hl.ptr<!hl.int>>
-// CHECK:      %2 = macroni.parameter "p" : !hl.lvalue<!hl.ptr<!hl.int>> {
-// CHECK:        %6 = hl.ref %0 : (!hl.lvalue<!hl.ptr<!hl.int>>) -> !hl.lvalue<!hl.ptr<!hl.int>>
-// CHECK:        hl.value.yield %6 : !hl.lvalue<!hl.ptr<!hl.int>>
-// CHECK:      }
-// CHECK:      %3 = macroni.parameter "v" : !hl.lvalue<!hl.ptr<!hl.int>> {
-// CHECK:        %6 = hl.ref %1 : (!hl.lvalue<!hl.ptr<!hl.int>>) -> !hl.lvalue<!hl.ptr<!hl.int>>
-// CHECK:        hl.value.yield %6 : !hl.lvalue<!hl.ptr<!hl.int>>
-// CHECK:      }
-// CHECK:      %4 = kernel.rcu_assign_pointer(%2, %3) : (!hl.lvalue<!hl.ptr<!hl.int>>, !hl.lvalue<!hl.ptr<!hl.int>>) -> !hl.ptr<!hl.int>
-// CHECK:      %5 = hl.const #core.integer<0> : !hl.int
-// CHECK:      hl.return %5 : !hl.int
-// CHECK:    }
-// CHECK:  }
-// CHECK:}
-
--- test/KernelTests/rcu_dereference.c
+++ test/KernelTests/rcu_dereference.c
@@ -1,122 +1,14 @@
-// RUN: kernelize -x c %s | FileCheck %s --match-full-lines
+#include "rcu.h"
 
-#define rcu_dereference(p) (*p)
-#define rcu_dereference_bh(p) (*p)
-#define rcu_dereference_sched(p) (*p)
-#define rcu_dereference_check(p, c) (c && *p)
-#define rcu_dereference_bh_check(p, c) (c && *p)
-#define rcu_dereference_sched_check(p, c) (c && *p)
-#define rcu_dereference_protected(p, c) (c && *p)
-
-#define deref(p) (rcu_dereference(p))
+struct string {
+  char *begin;
+  unsigned long size;
+};
 
 int main(void) {
-        int *p = (int *) 0xC0FFEE;
-        rcu_dereference(p);
-        rcu_dereference_bh(p);
-        rcu_dereference_sched(p);
-        rcu_dereference_check(p, 1);
-        rcu_dereference_bh_check(p, 1);
-        rcu_dereference_sched_check(p, 1);
-        rcu_dereference_protected(p, 1);
-        deref(p);
-        return 0;
+  int *x = 0;
+  struct string *s = {0};
+  rcu_dereference(x);
+  rcu_dereference(s->begin);
+  return 0;
 }
-
-// CHECK:hl.translation_unit {
-// CHECK:  hl.typedef "__int128_t" : !hl.int128
-// CHECK:  hl.typedef "__uint128_t" : !hl.int128< unsigned >
-// CHECK:  hl.struct "__NSConstantString_tag" {type_visibility = #unsup<attr "type_visibility">} : {
-// CHECK:    hl.field "isa" : !hl.ptr<!hl.int< const >>
-// CHECK:    hl.field "flags" : !hl.int
-// CHECK:    hl.field "str" : !hl.ptr<!hl.char< const >>
-// CHECK:    hl.field "length" : !hl.long
-// CHECK:  }
-// CHECK:  hl.typedef "__NSConstantString" : !hl.record<"__NSConstantString_tag">
-// CHECK:  hl.typedef "__builtin_ms_va_list" : !hl.ptr<!hl.char>
-// CHECK:  hl.struct "__va_list_tag" {type_visibility = #unsup<attr "type_visibility">} : {
-// CHECK:    hl.field "gp_offset" : !hl.int< unsigned >
-// CHECK:    hl.field "fp_offset" : !hl.int< unsigned >
-// CHECK:    hl.field "overflow_arg_area" : !hl.ptr<!hl.void>
-// CHECK:    hl.field "reg_save_area" : !hl.ptr<!hl.void>
-// CHECK:  }
-// CHECK:  hl.typedef "__builtin_va_list" : !hl.array<1, !hl.record<"__va_list_tag">>
-// CHECK:  hl.func @main () -> !hl.int {
-// CHECK:    core.scope {
-// CHECK:      %0 = hl.var "p" : !hl.lvalue<!hl.ptr<!hl.int>> = {
-// CHECK:        %25 = hl.const #core.integer<12648430> : !hl.int
-// CHECK:        %26 = hl.cstyle_cast %25 IntegralToPointer : !hl.int -> !hl.ptr<!hl.int>
-// CHECK:        hl.value.yield %26 : !hl.ptr<!hl.int>
-// CHECK:      }
-// CHECK:      %1 = macroni.parameter "p" : !hl.lvalue<!hl.ptr<!hl.int>> {
-// CHECK:        %25 = hl.ref %0 : (!hl.lvalue<!hl.ptr<!hl.int>>) -> !hl.lvalue<!hl.ptr<!hl.int>>
-// CHECK:        hl.value.yield %25 : !hl.lvalue<!hl.ptr<!hl.int>>
-// CHECK:      }
-// CHECK:      %2 = kernel.rcu_dereference(%1) : (!hl.lvalue<!hl.ptr<!hl.int>>) -> !hl.lvalue<!hl.int>
-// CHECK:      %3 = hl.implicit_cast %2 LValueToRValue : !hl.lvalue<!hl.int> -> !hl.int
-// CHECK:      %4 = macroni.parameter "p" : !hl.lvalue<!hl.ptr<!hl.int>> {
-// CHECK:        %25 = hl.ref %0 : (!hl.lvalue<!hl.ptr<!hl.int>>) -> !hl.lvalue<!hl.ptr<!hl.int>>
-// CHECK:        hl.value.yield %25 : !hl.lvalue<!hl.ptr<!hl.int>>
-// CHECK:      }
-// CHECK:      %5 = kernel.rcu_dereference_bh(%4) : (!hl.lvalue<!hl.ptr<!hl.int>>) -> !hl.lvalue<!hl.int>
-// CHECK:      %6 = hl.implicit_cast %5 LValueToRValue : !hl.lvalue<!hl.int> -> !hl.int
-// CHECK:      %7 = macroni.parameter "p" : !hl.lvalue<!hl.ptr<!hl.int>> {
-// CHECK:        %25 = hl.ref %0 : (!hl.lvalue<!hl.ptr<!hl.int>>) -> !hl.lvalue<!hl.ptr<!hl.int>>
-// CHECK:        hl.value.yield %25 : !hl.lvalue<!hl.ptr<!hl.int>>
-// CHECK:      }
-// CHECK:      %8 = kernel.rcu_dereference_sched(%7) : (!hl.lvalue<!hl.ptr<!hl.int>>) -> !hl.lvalue<!hl.int>
-// CHECK:      %9 = hl.implicit_cast %8 LValueToRValue : !hl.lvalue<!hl.int> -> !hl.int
-// CHECK:      %10 = macroni.parameter "p" : !hl.lvalue<!hl.ptr<!hl.int>> {
-// CHECK:        %25 = hl.ref %0 : (!hl.lvalue<!hl.ptr<!hl.int>>) -> !hl.lvalue<!hl.ptr<!hl.int>>
-// CHECK:        hl.value.yield %25 : !hl.lvalue<!hl.ptr<!hl.int>>
-// CHECK:      }
-// CHECK:      %11 = macroni.parameter "c" : !hl.int {
-// CHECK:        %25 = hl.const #core.integer<1> : !hl.int
-// CHECK:        hl.value.yield %25 : !hl.int
-// CHECK:      }
-// CHECK:      %12 = kernel.rcu_dereference_check(%10, %11) : (!hl.lvalue<!hl.ptr<!hl.int>>, !hl.int) -> !hl.int
-// CHECK:      %13 = macroni.parameter "p" : !hl.lvalue<!hl.ptr<!hl.int>> {
-// CHECK:        %25 = hl.ref %0 : (!hl.lvalue<!hl.ptr<!hl.int>>) -> !hl.lvalue<!hl.ptr<!hl.int>>
-// CHECK:        hl.value.yield %25 : !hl.lvalue<!hl.ptr<!hl.int>>
-// CHECK:      }
-// CHECK:      %14 = macroni.parameter "c" : !hl.int {
-// CHECK:        %25 = hl.const #core.integer<1> : !hl.int
-// CHECK:        hl.value.yield %25 : !hl.int
-// CHECK:      }
-// CHECK:      %15 = kernel.rcu_dereference_bh_check(%13, %14) : (!hl.lvalue<!hl.ptr<!hl.int>>, !hl.int) -> !hl.int
-// CHECK:      %16 = macroni.parameter "p" : !hl.lvalue<!hl.ptr<!hl.int>> {
-// CHECK:        %25 = hl.ref %0 : (!hl.lvalue<!hl.ptr<!hl.int>>) -> !hl.lvalue<!hl.ptr<!hl.int>>
-// CHECK:        hl.value.yield %25 : !hl.lvalue<!hl.ptr<!hl.int>>
-// CHECK:      }
-// CHECK:      %17 = macroni.parameter "c" : !hl.int {
-// CHECK:        %25 = hl.const #core.integer<1> : !hl.int
-// CHECK:        hl.value.yield %25 : !hl.int
-// CHECK:      }
-// CHECK:      %18 = kernel.rcu_dereference_sched_check(%16, %17) : (!hl.lvalue<!hl.ptr<!hl.int>>, !hl.int) -> !hl.int
-// CHECK:      %19 = macroni.parameter "p" : !hl.lvalue<!hl.ptr<!hl.int>> {
-// CHECK:        %25 = hl.ref %0 : (!hl.lvalue<!hl.ptr<!hl.int>>) -> !hl.lvalue<!hl.ptr<!hl.int>>
-// CHECK:        hl.value.yield %25 : !hl.lvalue<!hl.ptr<!hl.int>>
-// CHECK:      }
-// CHECK:      %20 = macroni.parameter "c" : !hl.int {
-// CHECK:        %25 = hl.const #core.integer<1> : !hl.int
-// CHECK:        hl.value.yield %25 : !hl.int
-// CHECK:      }
-// CHECK:      %21 = kernel.rcu_dereference_protected(%19, %20) : (!hl.lvalue<!hl.ptr<!hl.int>>, !hl.int) -> !hl.int
-// CHECK:      %22 = macroni.expansion "deref(p)" : !hl.lvalue<!hl.int> {
-// CHECK:        %25 = hl.expr : !hl.lvalue<!hl.int> {
-// CHECK:          %26 = macroni.parameter "p" : !hl.lvalue<!hl.ptr<!hl.int>> {
-// CHECK:            %28 = hl.ref %0 : (!hl.lvalue<!hl.ptr<!hl.int>>) -> !hl.lvalue<!hl.ptr<!hl.int>>
-// CHECK:            hl.value.yield %28 : !hl.lvalue<!hl.ptr<!hl.int>>
-// CHECK:          }
-// CHECK:          %27 = kernel.rcu_dereference(%26) : (!hl.lvalue<!hl.ptr<!hl.int>>) -> !hl.lvalue<!hl.int>
-// CHECK:          hl.value.yield %27 : !hl.lvalue<!hl.int>
-// CHECK:        }
-// CHECK:        hl.value.yield %25 : !hl.lvalue<!hl.int>
-// CHECK:      }
-// CHECK:      %23 = hl.implicit_cast %22 LValueToRValue : !hl.lvalue<!hl.int> -> !hl.int
-// CHECK:      %24 = hl.const #core.integer<0> : !hl.int
-// CHECK:      hl.return %24 : !hl.int
-// CHECK:    }
-// CHECK:  }
-// CHECK:}
-
