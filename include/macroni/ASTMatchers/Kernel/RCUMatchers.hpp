47c2cab810d1daf966bc9066fdb8e4a53252922d
Brent Pappas <pappasbrent@gmail.com>
2024-06-17 15:53:01 -0400

    Define AST matcher for rcu_replace_pointer()
    - Define AST matcher for rcu_replace_pointer().
    - Add a field to the rcu_collector to track matched invocations of
      rcu_replace_pointer().

--- include/macroni/ASTMatchers/Kernel/RCUMatchers.hpp
+++ include/macroni/ASTMatchers/Kernel/RCUMatchers.hpp
@@ -2,7 +2,6 @@
 
 #include "macroni/Translation/Kernel/KernelVisitor.hpp"
 #include <clang/AST/ASTContext.h>
-#include <clang/AST/Expr.h>
 #include <clang/ASTMatchers/ASTMatchFinder.h>
 #include <clang/ASTMatchers/ASTMatchers.h>
 
@@ -56,6 +55,69 @@ static const clang::ast_matchers::StatementMatcher rcu_access_pointer_matcher =
                         clang::ast_matchers::expr().bind("p"))))))))
         .bind("rcu_access_pointer");
 
+static const clang::ast_matchers::StatementMatcher rcu_replace_pointer_matcher =
+    clang::ast_matchers::stmtExpr(
+        clang::ast_matchers::allOf(
+            clang::ast_matchers::isExpandedFromMacro("rcu_replace_pointer"),
+
+            clang::ast_matchers::hasDescendant(clang::ast_matchers::callExpr(
+                clang::ast_matchers::callee(clang::ast_matchers::functionDecl(
+                    clang::ast_matchers::hasName("__write_once_size"))),
+                // NOTE(Brent): We have to use hasAnyArgument() instead of
+                // hasArgument() because for some reason Clang implicitly
+                // ignores parentheseses for the latter matcher.
+                clang::ast_matchers::hasAnyArgument(
+                    clang::ast_matchers::ignoringImplicit(
+                        clang::ast_matchers::unaryOperator(
+                            clang::ast_matchers::hasOperatorName("&"),
+                            clang::ast_matchers::has(
+                                clang::ast_matchers::parenExpr(
+                                    clang::ast_matchers::has(
+                                        clang::ast_matchers::parenExpr(
+                                            clang::ast_matchers::has(
+                                                clang::ast_matchers::parenExpr(
+                                                    clang::ast_matchers::has(
+                                                        clang::ast_matchers::
+                                                            expr()
+                                                                .bind("rcu_ptr")
+
+                                                            ))))))))))
+
+                    )),
+
+            clang::ast_matchers::hasDescendant(clang::ast_matchers::callExpr(
+                clang::ast_matchers::callee(clang::ast_matchers::functionDecl(
+                    clang::ast_matchers::hasName("__builtin_constant_p"))),
+                clang::ast_matchers::hasAnyArgument(
+                    clang::ast_matchers::ignoringImplicit(
+                        clang::ast_matchers::parenExpr(clang::ast_matchers::has(
+                            clang::ast_matchers::expr().bind("ptr"))))))),
+
+            clang::ast_matchers::hasDescendant(clang::ast_matchers::binaryOperator(
+                clang::ast_matchers::hasOperatorName("&&"),
+                clang::ast_matchers::hasLHS(
+                    clang::ast_matchers::ignoringImplicit(
+                        clang::ast_matchers::integerLiteral(
+                            clang::ast_matchers::equals(0)))),
+                clang::ast_matchers::hasRHS(clang::ast_matchers::ignoringImplicit(
+                    clang::ast_matchers::parenExpr(clang::ast_matchers::has(
+                        clang::ast_matchers::ignoringImplicit(clang::ast_matchers::unaryOperator(
+                            clang::ast_matchers::hasOperatorName("!"),
+                            clang::ast_matchers::hasUnaryOperand(
+                                clang::ast_matchers::ignoringImplicit(
+                                    clang::ast_matchers::parenExpr(clang::ast_matchers::has(
+                                        clang::ast_matchers::parenExpr(
+                                            clang::ast_matchers::has(
+                                                clang::ast_matchers::parenExpr(
+                                                    clang::ast_matchers::has(
+                                                        clang::ast_matchers::expr()
+                                                            .bind(
+                                                                "c")))))))))))))))))
+
+                ))
+
+        .bind("rcu_replace_pointer");
+
 class rcu_collector : public clang::ast_matchers::MatchFinder::MatchCallback {
 public:
   virtual void
@@ -64,5 +126,6 @@ public:
   rcu_dereference_table m_rcu_dereference_to_p;
   rcu_assign_pointer_table m_rcu_assign_pointer_to_params;
   rcu_access_pointer_table m_rcu_access_pointer_to_p;
+  rcu_replace_pointer_table m_rcu_replace_pointer_to_params;
 };
 } // namespace macroni::kernel
