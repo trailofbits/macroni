4713ae1ae8aee236be514f5860a2a7a7f036593c
Brent Pappas <pappasbrent@gmail.com>
2024-06-17 12:47:30 -0400

    Remove MLIRUtils
    Remove MLIRUtils since it's only contribution to the project is the
    `build_region()` function, which VAST already defines.

--- include/macroni/Common/MLIRUtils.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "vast/Util/Common.hpp"
-
-namespace macroni {
-void build_region(vast::mlir_builder &bld, vast::op_state &st,
-                  vast::builder_callback_ref callback);
-} // namespace macroni
