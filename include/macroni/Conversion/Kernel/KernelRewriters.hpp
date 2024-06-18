8a293a6a4507e2e10db514d7524fff7c7a226e86
Brent Pappas <pappasbrent@gmail.com>
2024-06-18 12:48:51 -0400

    Delete commented-out function declarations

--- include/macroni/Conversion/Kernel/KernelRewriters.hpp
+++ include/macroni/Conversion/Kernel/KernelRewriters.hpp
@@ -5,40 +5,6 @@
 #include <mlir/Support/LogicalResult.h>
 
 namespace macroni::kernel {
-// mlir::LogicalResult rewrite_get_user(macroni::MacroExpansion exp,
-//                                      mlir::PatternRewriter &rewriter);
-
-// mlir::LogicalResult rewrite_offsetof(macroni::MacroExpansion exp,
-//                                      mlir::PatternRewriter &rewriter);
-
-// mlir::LogicalResult rewrite_container_of(macroni::MacroExpansion exp,
-//                                          mlir::PatternRewriter &rewriter);
-
-// mlir::LogicalResult rewrite_rcu_dereference(macroni::MacroExpansion exp,
-//                                             mlir::PatternRewriter &rewriter);
-
-// mlir::LogicalResult
-// rewrite_rcu_dereference_check(macroni::MacroExpansion exp,
-//                               mlir::PatternRewriter &rewriter);
-
-// mlir::LogicalResult rewrite_smp_mb(macroni::MacroExpansion exp,
-//                                    mlir::PatternRewriter &rewriter);
-
-// mlir::LogicalResult rewrite_rcu_access_pointer(macroni::MacroExpansion exp,
-//                                                mlir::PatternRewriter
-//                                                &rewriter);
-
-// mlir::LogicalResult rewrite_rcu_assign_pointer(macroni::MacroExpansion exp,
-//                                                mlir::PatternRewriter
-//                                                &rewriter);
-
-// mlir::LogicalResult
-// rewrite_rcu_replace_pointer(macroni::MacroExpansion exp,
-//                             mlir::PatternRewriter &rewriter);
-
-// mlir::LogicalResult rewrite_list_for_each(vast::hl::ForOp for_op,
-//                                           mlir::PatternRewriter &rewriter);
-
 mlir::LogicalResult rewrite_label_stmt(vast::hl::LabelStmt label_stmt,
                                        mlir::PatternRewriter &rewriter);
 
