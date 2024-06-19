316f5afa59f620562efd3d5cba31734d47ead389
Brent Pappas <pappasbrent@gmail.com>
2024-06-19 18:28:14 -0400

    Define RCU AST matchers
    - Define RCU AST matchers in source file.
    - Add `using namespace clang::ast_matchers` to define the matchers more
      cleanly.
    - Remove `inline` keyword from `make_rcu_dereference_matcher()`
      declaration because the compiler can tell when to inline better than I
      can anyway.

--- lib/macroni/ASTMatchers/Kernel/RCUMatchers.cpp
+++ lib/macroni/ASTMatchers/Kernel/RCUMatchers.cpp
@@ -6,8 +6,87 @@
 #include <clang/ASTMatchers/ASTMatchers.h>
 
 namespace macroni::kernel {
-void rcu_collector::run(
-    const clang::ast_matchers::MatchFinder::MatchResult &Result) {
+using namespace clang::ast_matchers;
+
+const StatementMatcher
+rcu_collector::make_rcu_dereference_matcher(std::string suffix) {
+  auto name = "rcu_dereference" + suffix;
+  return stmtExpr(allOf(isExpandedFromMacro(name),
+                        hasDescendant(
+                            parenExpr(has(parenExpr(has(expr().bind("p"))))))))
+      .bind(name);
+}
+
+const StatementMatcher rcu_collector::rcu_deference_matcher =
+    make_rcu_dereference_matcher("");
+
+const StatementMatcher rcu_collector::rcu_deference_bh_matcher =
+    make_rcu_dereference_matcher("_bh");
+
+const StatementMatcher rcu_collector::rcu_deference_sched_matcher =
+    make_rcu_dereference_matcher("_sched");
+
+const StatementMatcher rcu_collector::rcu_assign_pointer_matcher =
+    doStmt(allOf(isExpandedFromMacro("rcu_assign_pointer"),
+                 hasDescendant(callExpr(
+                     allOf(callee(functionDecl(hasName("__write_once_size"))),
+                           hasArgument(0, hasDescendant(parenExpr(has(parenExpr(
+                                              has(expr().bind("p")))))))))),
+                 hasDescendant(callExpr(allOf(
+                     callee(functionDecl(hasName("__builtin_constant_p"))),
+                     hasArgument(0, expr().bind("v")))))
+
+                     ))
+        .bind("rcu_assign_pointer");
+
+const StatementMatcher rcu_collector::rcu_access_pointer_matcher =
+    stmtExpr(
+        allOf(isExpandedFromMacro("rcu_access_pointer"),
+              hasDescendant(parenExpr(has(parenExpr(has(expr().bind("p"))))))))
+        .bind("rcu_access_pointer");
+
+const StatementMatcher rcu_collector::rcu_replace_pointer_matcher =
+    stmtExpr(
+        allOf(isExpandedFromMacro("rcu_replace_pointer"),
+
+              hasDescendant(
+                  callExpr(callee(functionDecl(hasName("__write_once_size"))),
+                           // NOTE(Brent): We have to use hasAnyArgument()
+                           // instead of hasArgument() because for some reason
+                           // Clang implicitly ignores parentheseses for the
+                           // latter matcher.
+                           hasAnyArgument(ignoringImplicit(unaryOperator(
+                               hasOperatorName("&"),
+                               has(parenExpr(has(parenExpr(has(parenExpr(has(
+
+                                   expr().bind("rc"
+                                               "u_"
+                                               "pt"
+                                               "r")
+
+                                       ))))))))))
+
+                               )),
+
+              hasDescendant(callExpr(
+                  callee(functionDecl(hasName("__builtin_constant_p"))),
+                  hasAnyArgument(
+                      ignoringImplicit(parenExpr(has(expr().bind("ptr"))))))),
+
+              hasDescendant(binaryOperator(
+                  hasOperatorName("&&"),
+                  hasLHS(ignoringImplicit(integerLiteral(equals(0)))),
+                  hasRHS(ignoringImplicit(parenExpr(has(ignoringImplicit(
+                      unaryOperator(hasOperatorName("!"),
+                                    hasUnaryOperand(ignoringImplicit(
+                                        parenExpr(has(parenExpr(has(parenExpr(
+                                            has(expr().bind("c")))))))))))))))))
+
+                  ))
+
+        .bind("rcu_replace_pointer");
+
+void rcu_collector::run(const MatchFinder::MatchResult &Result) {
   if (auto rcu_dereference =
           Result.Nodes.getNodeAs<clang::StmtExpr>("rcu_dereference")) {
     auto p = Result.Nodes.getNodeAs<clang::Expr>("p");
