33e031c4a24c8cc816aa4ceeb523a628529f2c76
Brent Pappas <pappasbrent@gmail.com>
2024-06-20 14:56:35 -0400

    Shorten variable names
    Shorten names of variables containing `argument` and `parameter`.

--- lib/macroni/ASTMatchers/Kernel/RCUMatchers.cpp
+++ lib/macroni/ASTMatchers/Kernel/RCUMatchers.cpp
@@ -100,12 +100,12 @@ void rcu_collector::attach_to(clang::ast_matchers::MatchFinder &finder) {
 void rcu_collector::run(const MatchFinder::MatchResult &Result) {
   for (auto rcu_macro : KernelDialect::rcu_macro_spellings) {
     if (auto expansion = Result.Nodes.getNodeAs<clang::Stmt>(rcu_macro.name)) {
-      std::vector<const clang::Expr *> arguments;
-      for (auto parameter_name : rcu_macro.parameter_names) {
-        auto argument = Result.Nodes.getNodeAs<clang::Expr>(parameter_name);
-        arguments.push_back(argument);
+      std::vector<const clang::Expr *> args;
+      for (auto param_name : rcu_macro.parameter_names) {
+        auto arg = Result.Nodes.getNodeAs<clang::Expr>(param_name);
+        args.push_back(arg);
       }
-      m_expansions.insert({expansion, {rcu_macro, arguments}});
+      m_expansions.insert({expansion, {rcu_macro, args}});
     }
   }
 }
