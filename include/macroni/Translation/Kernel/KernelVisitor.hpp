e6c49870f439b62134439b141b17ff296fd38589
Brent Pappas <pappasbrent@gmail.com>
2024-06-20 13:20:26 -0400

    Streamline expansion logic with expansion table
    - Replace per-macro expansion mappings with single expansion table.
    - Remove per-macro visitor methods.

--- include/macroni/Translation/Kernel/KernelVisitor.hpp
+++ include/macroni/Translation/Kernel/KernelVisitor.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "macroni/Common/EmptyVisitor.hpp"
+#include "macroni/Common/ExpansionTable.hpp"
+#include "macroni/Common/MacroSpelling.hpp"
 #include "vast/CodeGen/CodeGenBuilder.hpp"
 #include "vast/CodeGen/CodeGenMeta.hpp"
 #include "vast/CodeGen/CodeGenVisitorBase.hpp"
@@ -13,66 +15,24 @@
 #include <clang/AST/Stmt.h>
 #include <mlir/IR/Operation.h>
 #include <optional>
-#include <unordered_map>
 
 namespace macroni::kernel {
-using rcu_dereference_table =
-    std::unordered_map<const clang::StmtExpr *, const clang::Expr *>;
-
-struct rcu_assign_pointer_parameters {
-  const clang::Expr *p;
-  const clang::Expr *v;
-};
-
-struct rcu_replace_pointer_parameters {
-  const clang::Expr *rcu_ptr;
-  const clang::Expr *ptr;
-  const clang::Expr *c;
-};
-
-using rcu_assign_pointer_table =
-    std::unordered_map<const clang::DoStmt *, rcu_assign_pointer_parameters>;
-
-using rcu_access_pointer_table =
-    std::unordered_map<const clang::StmtExpr *, const clang::Expr *>;
-
-using rcu_replace_pointer_table =
-    std::unordered_map<const clang::StmtExpr *, rcu_replace_pointer_parameters>;
 
 struct kernel_visitor : ::macroni::empty_visitor {
-  [[nodiscard]] explicit kernel_visitor(
-      rcu_dereference_table &rcu_dereference_to_p,
-      rcu_dereference_table &rcu_dereference_bh_to_p,
-      rcu_dereference_table &rcu_dereference_sched_to_p,
-      rcu_assign_pointer_table &rcu_assign_pointer_params,
-      rcu_access_pointer_table &rcu_access_pointer_to_p,
-      rcu_replace_pointer_table &m_rcu_replace_pointer_to_params,
-      vast::acontext_t &actx, vast::mcontext_t &mctx,
-      vast::cg::codegen_builder &bld, vast::cg::meta_generator &mg,
-      vast::cg::symbol_generator &sg, vast::cg::visitor_view view);
+  [[nodiscard]] explicit kernel_visitor(expansion_table &expansions,
+                                        vast::acontext_t &actx,
+                                        vast::mcontext_t &mctx,
+                                        vast::cg::codegen_builder &bld,
+                                        vast::cg::meta_generator &mg,
+                                        vast::cg::symbol_generator &sg,
+                                        vast::cg::visitor_view view);
 
   [[nodiscard]] vast::operation visit(const vast::cg::clang_stmt *stmt,
                                       vast::cg::scope_context &scope) override;
 
   [[nodiscard]] std::optional<vast::operation>
-  visit_rcu_dereference(const vast::cg::clang_stmt *stmt,
-                        vast::cg::scope_context &scope);
-
-  [[nodiscard]] std::optional<vast::operation>
   visit_rcu_read_lock_or_unlock(const vast::cg::clang_stmt *stmt,
                                 vast::cg::scope_context &scope);
-
-  [[nodiscard]] std::optional<vast::operation>
-  visit_rcu_assign_pointer(const vast::cg::clang_stmt *stmt,
-                           vast::cg::scope_context &scope);
-
-  [[nodiscard]] std::optional<vast::operation>
-  visit_rcu_access_pointer(const vast::cg::clang_stmt *stmt,
-                           vast::cg::scope_context &scope);
-
-  [[nodiscard]] std::optional<vast::operation>
-  visit_rcu_replace_pointer(const vast::cg::clang_stmt *stmt,
-                            vast::cg::scope_context &scope);
 
   [[nodiscard]] vast::operation visit(const vast::cg::clang_decl *decl,
                                       vast::cg::scope_context &scope) override;
@@ -83,13 +43,7 @@ struct kernel_visitor : ::macroni::empty_visitor {
 
   void unlock_op(mlir::Operation &op);
 
-  rcu_dereference_table &m_rcu_dereference_to_p;
-  rcu_dereference_table &m_rcu_dereference_bh_to_p;
-  rcu_dereference_table &m_rcu_dereference_sched_to_p;
-  rcu_assign_pointer_table &m_rcu_assign_pointer_params;
-  rcu_access_pointer_table &m_rcu_access_pointer_to_p;
-  rcu_replace_pointer_table &m_rcu_replace_pointer_to_params;
-
+  expansion_table &m_expansions;
   vast::acontext_t &m_actx;
   vast::cg::codegen_builder &m_bld;
   vast::cg::visitor_view m_view;
