c29350a7707ecd62f362d9d3044a398c04b2ada7
Brent Pappas <pappasbrent@gmail.com>
2024-06-20 13:17:18 -0400

    Replace expansion mappings with an expansion table
    Replace expansion mappings with an expansion table to allow for easy
    iteration and access to all matched macros. This assumes that no two
    macros expand to the same statement (which is true for the macros we
    currently match against).

--- include/macroni/ASTMatchers/Kernel/RCUMatchers.hpp
+++ include/macroni/ASTMatchers/Kernel/RCUMatchers.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "macroni/Translation/Kernel/KernelVisitor.hpp"
+#include "macroni/Common/ExpansionTable.hpp"
 #include <clang/AST/ASTContext.h>
 #include <clang/ASTMatchers/ASTMatchFinder.h>
 #include <clang/ASTMatchers/ASTMatchers.h>
@@ -25,11 +25,6 @@ public:
   virtual void
   run(const clang::ast_matchers::MatchFinder::MatchResult &Result) override;
 
-  rcu_dereference_table m_rcu_dereference_to_p;
-  rcu_dereference_table m_rcu_dereference_bh_to_p;
-  rcu_dereference_table m_rcu_dereference_sched_to_p;
-  rcu_assign_pointer_table m_rcu_assign_pointer_to_params;
-  rcu_access_pointer_table m_rcu_access_pointer_to_p;
-  rcu_replace_pointer_table m_rcu_replace_pointer_to_params;
+  expansion_table m_expansions;
 };
 } // namespace macroni::kernel
