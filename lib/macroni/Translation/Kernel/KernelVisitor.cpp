aa5adf8d40f21ef897d4d4bc0280e05b2009aaf3
Brent Pappas <pappasbrent@gmail.com>
2024-06-20 15:13:54 -0400

    Branch on number of arguments
    Branch on number of arguments.
    We do this for ops with 2 and 3 arguments as well because even though
    there are currently only one ops with each of these numbers of
    arguments, it provides a framework for adding more ops with 2 or 3
    arguments later.

--- lib/macroni/Translation/Kernel/KernelVisitor.cpp
+++ lib/macroni/Translation/Kernel/KernelVisitor.cpp
@@ -53,10 +53,8 @@ vast::operation kernel_visitor::visit(const vast::cg::clang_stmt *stmt,
   }();
 
   auto name = expansion.spelling.name;
-  if (KernelDialect::rcu_access_pointer.name == name ||
-      KernelDialect::rcu_dereference.name == name ||
-      KernelDialect::rcu_dereference_bh.name == name ||
-      KernelDialect::rcu_dereference_sched.name == name) {
+  auto num_args = expansion.arguments.size();
+  if (1 == num_args) {
     auto p = m_view.visit(expansion.arguments[0], scope)->getResult(0);
 
     if (KernelDialect::rcu_access_pointer.name == name) {
@@ -71,12 +69,14 @@ vast::operation kernel_visitor::visit(const vast::cg::clang_stmt *stmt,
     // if (KernelDialect::rcu_dereference_sched.name == name)
     return m_bld.create<RCUDereferenceSched>(loc, rty, p);
   }
-  if (KernelDialect::rcu_assign_pointer.name == name) {
+  if (2 == num_args) {
+    // KernelDialect::rcu_assign_pointer.name == name
     auto p = m_view.visit(expansion.arguments[0], scope)->getResult(0);
     auto v = m_view.visit(expansion.arguments[1], scope)->getResult(0);
     return m_bld.create<RCUAssignPointer>(loc, rty, p, v);
   }
-  if (KernelDialect::rcu_replace_pointer.name == name) {
+  if (3 == num_args) {
+    // KernelDialect::rcu_replace_pointer.name == name
     auto rcu_ptr = m_view.visit(expansion.arguments[0], scope)->getResult(0);
     auto ptr = m_view.visit(expansion.arguments[1], scope)->getResult(0);
     auto c = m_view.visit(expansion.arguments[2], scope)->getResult(0);
