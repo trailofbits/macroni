b8d94941897b86ad06bf2155855e4dd192728cee
Brent Pappas <pappasbrent@gmail.com>
2024-06-20 16:22:38 -0400

    Update test RUN line and expected output
    - Update test RUN line to reflect new way of running safe-c binary.
    - Update expected output to reflect new output for safe-c.

--- test/SafetyTests/unsafe.c
+++ test/SafetyTests/unsafe.c
@@ -1,4 +1,4 @@
-// RUN: safe-c -x c %s | FileCheck %s --match-full-lines
+// RUN: safe-c %s -- | FileCheck %s --match-full-lines
 
 #define unsafe if (0) ; else
 
@@ -9,32 +9,25 @@ int main(void) {
         return 0;
 }
 
-// CHECK:hl.translation_unit {
-// CHECK:  hl.typedef "__int128_t" : !hl.int128
-// CHECK:  hl.typedef "__uint128_t" : !hl.int128< unsigned >
-// CHECK:  hl.struct "__NSConstantString_tag" {type_visibility = #unsup<attr "type_visibility">} : {
-// CHECK:    hl.field "isa" : !hl.ptr<!hl.int< const >>
-// CHECK:    hl.field "flags" : !hl.int
-// CHECK:    hl.field "str" : !hl.ptr<!hl.char< const >>
-// CHECK:    hl.field "length" : !hl.long
-// CHECK:  }
-// CHECK:  hl.typedef "__NSConstantString" : !hl.record<"__NSConstantString_tag">
-// CHECK:  hl.typedef "__builtin_ms_va_list" : !hl.ptr<!hl.char>
-// CHECK:  hl.struct "__va_list_tag" {type_visibility = #unsup<attr "type_visibility">} : {
-// CHECK:    hl.field "gp_offset" : !hl.int< unsigned >
-// CHECK:    hl.field "fp_offset" : !hl.int< unsigned >
-// CHECK:    hl.field "overflow_arg_area" : !hl.ptr<!hl.void>
-// CHECK:    hl.field "reg_save_area" : !hl.ptr<!hl.void>
-// CHECK:  }
-// CHECK:  hl.typedef "__builtin_va_list" : !hl.array<1, !hl.record<"__va_list_tag">>
-// CHECK:  hl.func @main () -> !hl.int {
-// CHECK:    core.scope {
-// CHECK:      "safety.unsafe"() ({
-// CHECK:        hl.skip
-// CHECK:      }) : () -> ()
-// CHECK:      %0 = hl.const #core.integer<0> : !hl.int
-// CHECK:      hl.return %0 : !hl.int
-// CHECK:    }
-// CHECK:  }
-// CHECK:}
-
+// CHECK:   hl.translation_unit {
+// CHECK:     hl.typedef "__int128_t" : !hl.int128
+// CHECK:     hl.typedef "__uint128_t" : !hl.int128< unsigned >
+// CHECK:     hl.typedef "__NSConstantString" : !hl.record<"__NSConstantString_tag">
+// CHECK:     hl.typedef "__builtin_ms_va_list" : !hl.ptr<!hl.char>
+// CHECK:     hl.typedef "__builtin_va_list" : !hl.array<1, !hl.record<"__va_list_tag">>
+// CHECK:     hl.func @main external () -> !hl.int {
+// CHECK:       core.scope {
+// CHECK:         safety.unsafe {
+// CHECK:           core.scope {
+// CHECK:             %1 = hl.const #core.integer<1> : !hl.int
+// CHECK:           }
+// CHECK:         }
+// CHECK:         core.scope {
+// CHECK:           %1 = hl.const #core.integer<1> : !hl.int
+// CHECK:         }
+// CHECK:         %0 = hl.const #core.integer<0> : !hl.int
+// CHECK:         hl.return %0 : !hl.int
+// CHECK:       }
+// CHECK:     }
+// CHECK:   }
+// CHECK: }
