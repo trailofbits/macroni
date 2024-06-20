29dcfcce108eed51868cb05c98a899ed269ed980
Brent Pappas <pappasbrent@gmail.com>
2024-06-20 13:21:44 -0400

    Use expansion table to visit expansions
    Use the expansion table to visit all expansions in the stmt visitor
    instead of defining custom a visitor for each visitor method.
    If we could somehow group RCU MLIR operation types with their names and
    number of parameters then we could further improve this by iterating the
    RCU types and then calling the appropriate constructor when we find the
    name of the RCU op that the found expansion is an expansion of.

--- lib/macroni/Translation/Kernel/KernelVisitor.cpp
+++ lib/macroni/Translation/Kernel/KernelVisitor.cpp
@@ -1,5 +1,6 @@
 #include "macroni/Translation/Kernel/KernelVisitor.hpp"
 #include "macroni/Common/EmptyVisitor.hpp"
+#include "macroni/Common/ExpansionTable.hpp"
 #include "macroni/Dialect/Kernel/KernelAttributes.hpp"
 #include "macroni/Dialect/Kernel/KernelDialect.hpp"
 #include "macroni/Dialect/Kernel/KernelOps.hpp"
@@ -27,69 +28,55 @@
 #include <string_view>
 
 namespace macroni::kernel {
-kernel_visitor::kernel_visitor(
-    rcu_dereference_table &rcu_dereference_to_p,
-    rcu_dereference_table &rcu_dereference_bh_to_p,
-    rcu_dereference_table &rcu_dereference_sched_to_p,
-    rcu_assign_pointer_table &rcu_assign_pointer_params,
-    rcu_access_pointer_table &rcu_access_pointer_to_p,
-    rcu_replace_pointer_table &rcu_replace_pointer_to_params,
-    vast::acontext_t &actx, vast::mcontext_t &mctx,
-    vast::cg::codegen_builder &bld, vast::cg::meta_generator &mg,
-    vast::cg::symbol_generator &sg, vast::cg::visitor_view view)
-    : ::macroni::empty_visitor(mctx, mg, sg, view),
-      m_rcu_dereference_to_p(rcu_dereference_to_p),
-      m_rcu_dereference_bh_to_p(rcu_dereference_bh_to_p),
-      m_rcu_dereference_sched_to_p(rcu_dereference_sched_to_p),
-      m_rcu_assign_pointer_params(rcu_assign_pointer_params),
-      m_rcu_access_pointer_to_p(rcu_access_pointer_to_p),
-      m_rcu_replace_pointer_to_params(rcu_replace_pointer_to_params),
+kernel_visitor::kernel_visitor(expansion_table &expansions,
+                               vast::acontext_t &actx, vast::mcontext_t &mctx,
+                               vast::cg::codegen_builder &bld,
+                               vast::cg::meta_generator &mg,
+                               vast::cg::symbol_generator &sg,
+                               vast::cg::visitor_view view)
+    : ::macroni::empty_visitor(mctx, mg, sg, view), m_expansions(expansions),
       m_actx(actx), m_bld(bld), m_view(view) {}
 
 vast::operation kernel_visitor::visit(const vast::cg::clang_stmt *stmt,
                                       vast::cg::scope_context &scope) {
-  return visit_rcu_dereference(stmt, scope)
-      .or_else([&] { return visit_rcu_read_lock_or_unlock(stmt, scope); })
-      .or_else([&] { return visit_rcu_assign_pointer(stmt, scope); })
-      .or_else([&] { return visit_rcu_access_pointer(stmt, scope); })
-      .or_else([&] { return visit_rcu_replace_pointer(stmt, scope); })
-      // If we can't match anything, return nullptr
-      .value_or(nullptr);
-}
-
-std::optional<vast::operation>
-kernel_visitor::visit_rcu_dereference(const vast::cg::clang_stmt *stmt,
-                                      vast::cg::scope_context &scope) {
-  auto rcu_dereference = clang::dyn_cast<clang::StmtExpr>(stmt);
-  if (!rcu_dereference) {
-    return std::nullopt;
+  if (!m_expansions.contains(stmt)) {
+    return {};
   }
 
-  if (!m_rcu_dereference_to_p.contains(rcu_dereference) &&
-      !m_rcu_dereference_bh_to_p.contains(rcu_dereference) &&
-      !m_rcu_dereference_sched_to_p.contains(rcu_dereference)) {
-    return std::nullopt;
+  auto loc = m_view.location(stmt);
+  auto expansion = m_expansions.at(stmt);
+  vast::mlir_type rty = m_bld.void_type();
+  if (auto expr = clang::dyn_cast<clang::Expr>(stmt)) {
+    rty = m_view.visit(expr->getType(), scope);
   }
 
-  auto loc = m_view.location(rcu_dereference);
-  vast::cg::default_stmt_visitor visitor(m_bld, m_view, scope);
+  auto name = expansion.spelling.m_name;
+  if (KernelDialect::rcu_access_pointer.m_name == name ||
+      KernelDialect::rcu_dereference.m_name == name ||
+      KernelDialect::rcu_dereference_bh.m_name == name ||
+      KernelDialect::rcu_dereference_sched.m_name == name) {
+    auto p = m_view.visit(expansion.arguments[0], scope)->getResult(0);
 
-  if (m_rcu_dereference_to_p.contains(rcu_dereference)) {
-    auto p = m_rcu_dereference_to_p.at(rcu_dereference);
-    auto rty = m_view.visit(p->getType(), scope);
-    auto p_op = m_view.visit(p, scope);
-    return m_bld.create<RCUDereference>(loc, rty, p_op->getOpResult(0));
-  } else if (m_rcu_dereference_bh_to_p.contains(rcu_dereference)) {
-    auto p = m_rcu_dereference_bh_to_p.at(rcu_dereference);
-    auto rty = m_view.visit(p->getType(), scope);
-    auto p_op = m_view.visit(p, scope);
-    return m_bld.create<RCUDereferenceBH>(loc, rty, p_op->getOpResult(0));
+    if (KernelDialect::rcu_access_pointer.m_name == name) {
+      return m_bld.create<RCUAccessPointer>(loc, rty, p);
+    } else if (KernelDialect::rcu_dereference.m_name == name) {
+      return m_bld.create<RCUDereference>(loc, rty, p);
+    } else if (KernelDialect::rcu_dereference_bh.m_name == name) {
+      return m_bld.create<RCUDereferenceBH>(loc, rty, p);
+    } else if (KernelDialect::rcu_dereference_sched.m_name == name) {
+      return m_bld.create<RCUDereferenceSched>(loc, rty, p);
+    }
+  } else if (KernelDialect::rcu_assign_pointer.m_name == name) {
+    auto p = m_view.visit(expansion.arguments[0], scope)->getResult(0);
+    auto v = m_view.visit(expansion.arguments[1], scope)->getResult(0);
+    return m_bld.create<RCUAssignPointer>(loc, rty, p, v);
+  } else if (KernelDialect::rcu_replace_pointer.m_name == name) {
+    auto rcu_ptr = m_view.visit(expansion.arguments[0], scope)->getResult(0);
+    auto ptr = m_view.visit(expansion.arguments[1], scope)->getResult(0);
+    auto c = m_view.visit(expansion.arguments[2], scope)->getResult(0);
+    return m_bld.create<RCUReplacePointer>(loc, rty, rcu_ptr, ptr, c);
   }
-  // m_rcu_dereference_sched_to_p.contains(rcu_dereference);
-  auto p = m_rcu_dereference_sched_to_p.at(rcu_dereference);
-  auto rty = m_view.visit(p->getType(), scope);
-  auto p_op = m_view.visit(p, scope);
-  return m_bld.create<RCUDereferenceSched>(loc, rty, p_op->getOpResult(0));
+  return nullptr;
 }
 
 std::optional<vast::operation>
@@ -120,78 +107,6 @@ kernel_visitor::visit_rcu_read_lock_or_unlock(const vast::cg::clang_stmt *stmt,
   }
 
   return op;
-}
-
-std::optional<vast::operation>
-kernel_visitor::visit_rcu_assign_pointer(const vast::cg::clang_stmt *stmt,
-                                         vast::cg::scope_context &scope) {
-  auto rcu_assign_pointer = clang::dyn_cast<clang::DoStmt>(stmt);
-  if (!rcu_assign_pointer) {
-    return std::nullopt;
-  }
-
-  if (!m_rcu_assign_pointer_params.contains(rcu_assign_pointer)) {
-    return std::nullopt;
-  }
-
-  auto loc = m_view.location(rcu_assign_pointer);
-  auto rty = m_bld.void_type();
-  auto params = m_rcu_assign_pointer_params.at(rcu_assign_pointer);
-  auto p = params.p;
-  auto v = params.v;
-  auto visitor = vast::cg::default_stmt_visitor(m_bld, m_view, scope);
-  auto p_op = m_view.visit(p, scope);
-  auto v_op = m_view.visit(v, scope);
-  auto p_result = p_op->getOpResult(0);
-  auto v_result = v_op->getOpResult(0);
-
-  return m_bld.create<RCUAssignPointer>(loc, rty, p_result, v_result);
-}
-
-std::optional<vast::operation>
-kernel_visitor::visit_rcu_access_pointer(const vast::cg::clang_stmt *stmt,
-                                         vast::cg::scope_context &scope) {
-  auto rcu_access_pointer = clang::dyn_cast<clang::StmtExpr>(stmt);
-  if (!rcu_access_pointer) {
-    return std::nullopt;
-  }
-
-  if (!m_rcu_access_pointer_to_p.contains(rcu_access_pointer)) {
-    return std::nullopt;
-  }
-
-  auto loc = m_view.location(rcu_access_pointer);
-  auto p = m_rcu_access_pointer_to_p.at(rcu_access_pointer);
-  vast::cg::default_stmt_visitor visitor(m_bld, m_view, scope);
-  auto rty = m_view.visit(p->getType(), scope);
-  auto p_op = m_view.visit(p, scope);
-
-  return m_bld.create<RCUAccessPointer>(loc, rty, p_op->getOpResult(0));
-}
-
-std::optional<vast::operation>
-kernel_visitor::visit_rcu_replace_pointer(const vast::cg::clang_stmt *stmt,
-                                          vast::cg::scope_context &scope) {
-  auto rcu_replace_pointer = clang::dyn_cast<clang::StmtExpr>(stmt);
-  if (!rcu_replace_pointer) {
-    return std::nullopt;
-  }
-
-  if (!m_rcu_replace_pointer_to_params.contains(rcu_replace_pointer)) {
-    return std::nullopt;
-  }
-
-  auto loc = m_view.location(rcu_replace_pointer);
-  auto params = m_rcu_replace_pointer_to_params.at(rcu_replace_pointer);
-  vast::cg::default_stmt_visitor visitor(m_bld, m_view, scope);
-  auto rty = m_view.visit(params.rcu_ptr->getType(), scope);
-  auto rcu_ptr_op = m_view.visit(params.rcu_ptr, scope);
-  auto ptr_op = m_view.visit(params.ptr, scope);
-  auto c_op = m_view.visit(params.c, scope);
-
-  return m_bld.create<RCUReplacePointer>(loc, rty, rcu_ptr_op->getOpResult(0),
-                                         ptr_op->getOpResult(0),
-                                         c_op->getOpResult(0));
 }
 
 template <typename AttrT, typename... Rest>
