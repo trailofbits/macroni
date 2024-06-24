dde5e78789af47adce0a3dc653aa7193b0800f7d
Brent Pappas <pappasbrent@gmail.com>
2024-06-24 16:08:40 -0400

    Define kernel analysis pass
    Define kernel analysis pass for analyzing Linux kernel macro usage.
    Right now we only analyze RCU macro usage.

---
+++ lib/macroni/Analysis/Kernel/KernelAnalysisPass.cpp
@@ -0,0 +1,30 @@
+#include "macroni/Analysis/Kernel/KernelAnalysisPass.hpp"
+#include "macroni/Analysis/Kernel/RCUAnalysis.hpp"
+#include <llvm/ADT/StringRef.h>
+#include <memory>
+#include <mlir/Pass/Pass.h>
+#include <mlir/Pass/PassRegistry.h>
+
+namespace macroni::kernel {
+llvm::StringRef kernel_analysis_pass::getName() const { return "KernelCheck"; }
+
+std::unique_ptr<mlir::Pass> kernel_analysis_pass::clonePass() const {
+  return std::make_unique<kernel_analysis_pass>(*this);
+}
+
+llvm::StringRef kernel_analysis_pass::getArgument() const {
+  return "kernelcheck";
+}
+
+void kernel_analysis_pass::runOnOperation() {
+  // Mark all analyses as preserved. We do this to allow for caching analysis
+  // results. This is only valid because this operation pass does not perform
+  // any transformations.
+  markAllAnalysesPreserved();
+  getAnalysis<kernel_analysis>();
+}
+
+void register_kernel_analysis_pass() {
+  mlir::PassRegistration<kernel_analysis_pass>();
+}
+} // namespace macroni::kernel
