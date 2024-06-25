798e5f049b509f0fb02f75d898b0cbc9b31e9e0d
Brent Pappas <pappasbrent@gmail.com>
2024-06-25 09:18:40 -0400

    Add emit() method for emitting warnings
    - Add emit() method for emitting warnings. This is a virtual method so
      that users may override it with their own custom logic for emitting
      warnings; the default is to emit to llvm::errs().
    - Make other RCU analysis functions members of the kernel_analysis
      struct so that they can call emit().
    - Add virtual destructor.

--- include/macroni/Analysis/Kernel/RCUAnalysis.hpp
+++ include/macroni/Analysis/Kernel/RCUAnalysis.hpp
@@ -1,9 +1,24 @@
 #pragma once
 
+#include "macroni/Dialect/Kernel/KernelInterfaces.hpp"
+#include "macroni/Dialect/Kernel/KernelOps.hpp"
+#include "vast/Dialect/HighLevel/HighLevelOps.hpp"
+#include <llvm/Support/raw_ostream.h>
 #include <mlir/IR/Operation.h>
 
 namespace macroni::kernel {
 struct kernel_analysis {
   kernel_analysis(mlir::Operation *op);
+
+  virtual ~kernel_analysis(){};
+
+private:
+  void warn_rcu_dereference(RCU_Dereference_Interface &op);
+  void analyze_non_rcu_function(vast::hl::FuncOp &func);
+  auto walk_until_call(llvm::StringRef name);
+  void analyze_acquires_function(vast::hl::FuncOp &func);
+  void analyze_releases_function(vast::hl::FuncOp &func);
+  void analyze_critical_section(RCUCriticalSection cs);
+  virtual void emit(std::string warning) { llvm::errs() << warning; }
 };
 } // namespace macroni::kernel
