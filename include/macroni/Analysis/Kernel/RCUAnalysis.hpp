39f23c987cec033996285f6f015eb5f4cc8235b2
Brent Pappas <pappasbrent@gmail.com>
2024-06-25 10:11:48 -0400

    Pass operations of interest to hooks
    Pass operations of interest to hooks directly instead of just passing
    pre-defined warning messages to hooks so that users of the struct have
    more options for what to do when encountering interesting operations.

--- include/macroni/Analysis/Kernel/RCUAnalysis.hpp
+++ include/macroni/Analysis/Kernel/RCUAnalysis.hpp
@@ -3,8 +3,10 @@
 #include "macroni/Dialect/Kernel/KernelInterfaces.hpp"
 #include "macroni/Dialect/Kernel/KernelOps.hpp"
 #include "vast/Dialect/HighLevel/HighLevelOps.hpp"
+#include <format>
 #include <llvm/Support/raw_ostream.h>
 #include <mlir/IR/Operation.h>
+#include <string>
 
 namespace macroni::kernel {
 struct rcu_analysis {
@@ -13,12 +15,48 @@ struct rcu_analysis {
   virtual ~rcu_analysis() = default;
 
 protected:
-  void warn_rcu_dereference(RCU_Dereference_Interface &op);
   void analyze_non_rcu_function(vast::hl::FuncOp &func);
   auto walk_until_call(llvm::StringRef name);
   void analyze_acquires_function(vast::hl::FuncOp &func);
   void analyze_releases_function(vast::hl::FuncOp &func);
   void analyze_critical_section(RCUCriticalSection cs);
-  virtual void emit(std::string warning) { llvm::errs() << warning; }
+
+  // Format an operation's location as a string for diagnostics.
+  virtual std::string format_location(mlir::Operation *op) {
+    std::string s;
+    auto os = llvm::raw_string_ostream(s);
+    op->getLoc()->print(os);
+    s.erase(s.find("loc("), 4);
+    s.erase(s.find('"'), 1);
+    s.erase(s.find('"'), 1);
+    s.erase(s.rfind(')'), 1);
+    return s;
+  }
+
+  // Emit the given warning.
+  virtual void warn(std::string warning) { llvm::errs() << warning; }
+
+  // Emit the given information.
+  virtual void info(std::string information) { llvm::errs() << information; }
+
+  // Inform about a call to RCU access pointer inside a critical section.
+  virtual void
+  info_rcu_access_pointer_in_critical_section(RCUAccessPointer &op) {
+    auto information =
+        std::format("{}: info: Use rcu_dereference_protected() instead of "
+                    "rcu_access_pointer() in critical section\n",
+                    format_location(op));
+    info(information);
+  }
+
+  // Warn about an RCU dereference call outside a critical section.
+  virtual void warn_rcu_dereference_outside_critical_section(
+      RCU_Dereference_Interface &rcu_deref) {
+    auto op = rcu_deref.getOperation();
+    auto warning = std::format(
+        "{}: warning: Invocation of {}() outside of RCU critical section\n",
+        format_location(op), op->getName().stripDialect().str());
+    warn(warning);
+  }
 };
 } // namespace macroni::kernel
