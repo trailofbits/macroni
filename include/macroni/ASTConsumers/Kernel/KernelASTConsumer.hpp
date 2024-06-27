83a0c9aa5d5997f68723a6a3df79fe874d62a7a9
Brent Pappas <pappasbrent@gmail.com>
2024-06-27 08:54:24 -0400

    Replace print locations flag with module handler
    Replace print locations flag member with a module handler to provide
    callers with greater flexibility as to what to do with the lowered
    modules.

--- include/macroni/ASTConsumers/Kernel/KernelASTConsumer.hpp
+++ include/macroni/ASTConsumers/Kernel/KernelASTConsumer.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "macroni/Common/Common.hpp"
 #include <clang/AST/ASTConsumer.h>
 #include <clang/AST/ASTContext.h>
 #include <clang/ASTMatchers/ASTMatchFinder.h>
@@ -8,21 +9,21 @@
 namespace macroni::kernel {
 class KernelASTConsumer : public clang::ASTConsumer {
 public:
-  KernelASTConsumer(bool print_locations);
+  KernelASTConsumer(module_handler mod_handler);
 
   void HandleTranslationUnit(clang::ASTContext &Ctx) override;
 
 private:
-  bool m_print_locations;
+  module_handler m_module_handler;
 };
 
 class KernelASTConsumerFactory {
 public:
   std::unique_ptr<KernelASTConsumer> newASTConsumer(void);
 
-  KernelASTConsumerFactory(bool print_locations);
+  KernelASTConsumerFactory(module_handler mod_handler);
 
 private:
-  bool m_print_locations;
+  module_handler m_module_handler;
 };
 } // namespace macroni::kernel
