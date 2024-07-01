84b86dff81c3bc99b53ea17f9d364d48d3b58ddf
Brent Pappas <pappasbrent@gmail.com>
2024-07-01 14:19:24 -0400

    Update safety visitor to use fallback visitor
    - Update the safety visitor to inherit from vast's fallback visitor
      instead of macroni's old empty visitor.
    - Reorder the arguments in the visitor's constructor so that it can be
      created with a call to `vast::cg::as_node_with_list_ref()`.
    - Return `next->visit()` in case of failure instead of a `nullptr` so
      that nested nodes are visited starting from the start of the visitor
      list, and not starting only from the remaining visitors in the list.

--- lib/macroni/Translation/Safety/SafetyVisitor.cpp
+++ lib/macroni/Translation/Safety/SafetyVisitor.cpp
@@ -1,50 +1,43 @@
 #include "macroni/Translation/Safety/SafetyVisitor.hpp"
-#include "macroni/Common/EmptyVisitor.hpp"
 #include "macroni/Dialect/Safety/SafetyOps.hpp"
 #include "vast/CodeGen/CodeGenBuilder.hpp"
-#include "vast/CodeGen/CodeGenMeta.hpp"
 #include "vast/CodeGen/CodeGenVisitorBase.hpp"
+#include "vast/CodeGen/CodeGenVisitorList.hpp"
 #include "vast/CodeGen/Common.hpp"
 #include "vast/CodeGen/DefaultStmtVisitor.hpp"
-#include "vast/CodeGen/SymbolGenerator.hpp"
 #include "vast/Util/Common.hpp"
 #include <clang/AST/Expr.h>
 #include <clang/AST/Stmt.h>
 #include <clang/Basic/LLVM.h>
-#include <llvm/ADT/StringRef.h>
-#include <llvm/Support/raw_ostream.h>
 
 namespace macroni::safety {
-safety_visitor::safety_visitor(safety_conditions &safe_block_conditions,
+safety_visitor::safety_visitor(vast::cg::visitor_base &head,
                                vast::mcontext_t &mctx,
                                vast::cg::codegen_builder &bld,
-                               vast::cg::meta_generator &mg,
-                               vast::cg::symbol_generator &sg,
-                               vast::cg::visitor_view view)
-    : ::macroni::empty_visitor(mctx, mg, sg, view),
-      m_safe_block_conditions(safe_block_conditions), m_bld(bld), m_view(view) {
-}
+                               safety_conditions &safe_block_conditions)
+    : vast::cg::fallthrough_list_node(), m_mctx(mctx), m_bld(bld),
+      m_safe_block_conditions(safe_block_conditions), m_view(head) {}
 
 vast::operation safety_visitor::visit(const vast::cg::clang_stmt *stmt,
                                       vast::cg::scope_context &scope) {
   auto if_stmt = clang::dyn_cast<clang::IfStmt>(stmt);
   if (!if_stmt) {
-    return {};
+    return next->visit(stmt, scope);
   }
 
   auto else_branch = if_stmt->getElse();
   if (!else_branch) {
-    return {};
+    return next->visit(stmt, scope);
   }
 
   auto integer_literal =
       clang::dyn_cast<const clang::IntegerLiteral>(if_stmt->getCond());
   if (!integer_literal) {
-    return {};
+    return next->visit(stmt, scope);
   }
 
   if (!m_safe_block_conditions.contains(integer_literal)) {
-    return {};
+    return next->visit(stmt, scope);
   }
 
   auto mk_region_builder = [&](const vast::cg::clang_stmt *stmt) {
@@ -57,7 +50,7 @@ vast::operation safety_visitor::visit(const vast::cg::clang_stmt *stmt,
       .bind(mk_region_builder(else_branch))
       .freeze();
 
-  vast::cg::default_stmt_visitor visitor(m_bld, m_view, scope);
+  vast::cg::default_stmt_visitor visitor(m_mctx, m_bld, m_view, scope);
   auto op = visitor.visit(else_branch);
   return op;
 }
