7d79f6dcac82cd026da10cecef1ae06a4dffc853
Brent Pappas <pappasbrent@gmail.com>
2024-07-01 14:21:03 -0400

    Update Kernelize to use updated macroni visitor
    - Update Kernelize to use the updated kernel visitor and vast visitor
      list. Inline the logic for doing this for now for ease of
      understanding; the logic for creating the driver and emitting the
      module will be extracted into a function in a later commit.
    - Add error checking by verifying the codegen driver's module.

--- lib/macroni/ASTConsumers/Kernel/KernelASTConsumer.cpp
+++ lib/macroni/ASTConsumers/Kernel/KernelASTConsumer.cpp
@@ -1,18 +1,27 @@
 #include "macroni/ASTConsumers/Kernel/KernelASTConsumer.hpp"
 #include "macroni/ASTMatchers/Kernel/RCUMatchers.hpp"
-#include "macroni/Common/CodeGenDriverSetup.hpp"
 #include "macroni/Common/Common.hpp"
 #include "macroni/Common/MacroniMetaGenerator.hpp"
 #include "macroni/Conversion/Kernel/KernelRewriters.hpp"
 #include "macroni/Dialect/Kernel/KernelDialect.hpp"
 #include "macroni/Translation/Kernel/KernelVisitor.hpp"
+#include "vast/CodeGen/AttrVisitorProxy.hpp"
+#include "vast/CodeGen/CodeGenDriver.hpp"
+#include "vast/CodeGen/DefaultVisitor.hpp"
+#include "vast/CodeGen/FallthroughVisitor.hpp"
+#include "vast/CodeGen/TypeCachingProxy.hpp"
+#include "vast/CodeGen/UnsupportedVisitor.hpp"
+#include "vast/Dialect/Dialects.hpp"
 #include <clang/AST/ASTConsumer.h>
 #include <clang/AST/ASTContext.h>
 #include <clang/ASTMatchers/ASTMatchFinder.h>
 #include <memory>
+#include <mlir/IR/DialectRegistry.h>
 #include <mlir/IR/Iterators.h>
 #include <mlir/IR/OperationSupport.h>
+#include <mlir/InitAllDialects.h>
 #include <mlir/Transforms/GreedyPatternRewriteDriver.h>
+#include <utility>
 
 // NOTE(Brent): Not sure where to put this file's local functions. It may be
 // better to create some sort of "Analysis" module for them and place them
@@ -22,29 +31,59 @@ namespace macroni::kernel {
 KernelASTConsumer::KernelASTConsumer(module_handler mod_handler)
     : m_module_handler(mod_handler) {}
 
-void KernelASTConsumer::HandleTranslationUnit(clang::ASTContext &Ctx) {
+void KernelASTConsumer::HandleTranslationUnit(clang::ASTContext &actx) {
   // Match safe blocks.
 
   clang::ast_matchers::MatchFinder finder;
   rcu_collector matcher;
   matcher.attach_to(finder);
-  finder.matchAST(Ctx);
+  finder.matchAST(actx);
 
-  // Set up the driver.
+  // Load MLIR and VAST dialects into registry.
+  mlir::DialectRegistry registry;
+  mlir::registerAllDialects(registry);
+  vast::registerAllDialects(registry);
+
+  // Insert our custom dialect.
+  registry.insert<KernelDialect>();
+  auto mctx = std::make_unique<vast::mcontext_t>();
+  mctx->appendDialectRegistry(registry);
+  mctx->loadAllAvailableDialects();
+
+  // Set up variables for constructing the codegen driver.
+
+  auto bld = vast::cg::mk_codegen_builder(*mctx);
+  auto mg = std::make_shared<macroni::macroni_meta_generator>(&actx, &*mctx);
+  auto sg = std::make_shared<vast::cg::default_symbol_generator>(
+      actx.createMangleContext());
+
+  using vast::cg::as_node;
+  using vast::cg::as_node_with_list_ref;
+  auto visitors = std::make_shared<vast::cg::visitor_list>() |
+                  as_node_with_list_ref<kernel_visitor>(
+                      actx, *mctx, *bld, *mg, *sg, matcher.expansions) |
+                  as_node_with_list_ref<vast::cg::attr_visitor_proxy>() |
+                  as_node<vast::cg::type_caching_proxy>() |
+                  as_node_with_list_ref<vast::cg::default_visitor>(
+                      *mctx, *bld, mg, sg,
+                      /* strict return = */ false,
+                      vast::cg::missing_return_policy::emit_trap) |
+                  as_node_with_list_ref<vast::cg::unsup_visitor>(*mctx, *bld) |
+                  as_node<vast::cg::fallthrough_visitor>();
+
+  // Create the codegen driver.
   auto driver =
-      macroni::generate_codegen_driver<KernelDialect,
-                                       macroni::macroni_meta_generator>(Ctx);
+      std::make_unique<vast::cg::driver>(actx, *mctx, std::move(bld), visitors);
+  driver->enable_verifier(true);
 
-  driver->push_visitor(std::make_unique<kernel_visitor>(
-      matcher.expansions, Ctx, driver->mcontext(),
-      driver->get_codegen_builder(), driver->get_meta_generator(),
-      driver->get_symbol_generator(),
-      vast::cg::visitor_view(driver->get_visitor_stack())));
+  // Emit the lowered MLIR.
 
-  vast::cg::setup_default_visitor_stack(*driver);
-
-  driver->emit(Ctx.getTranslationUnitDecl());
+  driver->emit(actx.getTranslationUnitDecl());
   driver->finalize();
+  if (!driver->verify()) {
+    llvm::errs() << "Failed to verify driver\n";
+    return;
+  }
   auto mod = driver->freeze();
 
   // Rewrite rcu_read_lock()/unlock() calls into critical sections.
