a6d09438978146699eb4100a09e899ee156d5b60
Brent Pappas <pappasbrent@gmail.com>
2024-07-01 14:19:24 -0400

    Update safety visitor to use fallback visitor
    - Update the safety visitor to inherit from vast's fallback visitor
      instead of macroni's old empty visitor.
    - Reorder the arguments in the visitor's constructor so that it can be
      created with a call to `vast::cg::as_node_with_list_ref()`.
    - Return `next->visit()` in case of failure instead of a `nullptr` so
      that nested nodes are visited starting from the start of the visitor
      list, and not starting only from the remaining visitors in the list.

--- include/macroni/Translation/Safety/SafetyVisitor.hpp
+++ include/macroni/Translation/Safety/SafetyVisitor.hpp
@@ -1,12 +1,10 @@
 #pragma once
 
-#include "macroni/Common/EmptyVisitor.hpp"
 #include "vast/CodeGen/CodeGenBuilder.hpp"
-#include "vast/CodeGen/CodeGenMeta.hpp"
 #include "vast/CodeGen/CodeGenVisitorBase.hpp"
+#include "vast/CodeGen/CodeGenVisitorList.hpp"
 #include "vast/CodeGen/Common.hpp"
 #include "vast/CodeGen/ScopeContext.hpp"
-#include "vast/CodeGen/SymbolGenerator.hpp"
 #include "vast/Util/Common.hpp"
 #include <clang/AST/Expr.h>
 #include <unordered_set>
@@ -14,20 +12,20 @@
 namespace macroni::safety {
 using safety_conditions = std::unordered_set<const clang::IntegerLiteral *>;
 
-struct safety_visitor : macroni::empty_visitor {
+struct safety_visitor : vast::cg::fallthrough_list_node {
 
-  [[nodiscard]] safety_visitor(safety_conditions &safe_block_conditions,
+  [[nodiscard]] safety_visitor(vast::cg::visitor_base &head,
                                vast::mcontext_t &mctx,
                                vast::cg::codegen_builder &bld,
-                               vast::cg::meta_generator &mg,
-                               vast::cg::symbol_generator &sg,
-                               vast::cg::visitor_view view);
+                               safety_conditions &safe_block_conditions);
 
   [[nodiscard]] vast::operation visit(const vast::cg::clang_stmt *stmt,
                                       vast::cg::scope_context &scope) override;
 
-  safety_conditions &m_safe_block_conditions;
+protected:
+  vast::mcontext_t &m_mctx;
   vast::cg::codegen_builder &m_bld;
+  safety_conditions &m_safe_block_conditions;
   vast::cg::visitor_view m_view;
 };
 } // namespace macroni::safety
