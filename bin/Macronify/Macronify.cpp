6af19928a4782f3a09eb15fd79eebba1bc205c5e
Brent Pappas <pappasbrent@gmail.com>
2024-07-01 14:15:39 -0400

    Update Macronify to use updated macroni visitor
    - Update Macronify to use the updated macroni visitor and vast visitor
      list. Inline the logic for doing this for now for ease of
      understanding; the logic for creating the driver and emitting the
      module will be extracted into a function in a later commit.
    - Add error checking by verifying the codegen driver's module.

--- bin/Macronify/Macronify.cpp
+++ bin/Macronify/Macronify.cpp
@@ -5,13 +5,19 @@
 // LICENSE file found in the root directory of this source tree.
 
 #include "ParseAST.hpp"
-#include "macroni/Common/CodeGenDriverSetup.hpp"
 #include "macroni/Dialect/Macroni/MacroniDialect.hpp"
 #include "macroni/Translation/Macroni/MacroniVisitor.hpp"
 #include "macroni/Translation/Macroni/PastaMetaGenerator.hpp"
 #include "pasta/AST/AST.h"
+#include "vast/CodeGen/AttrVisitorProxy.hpp"
 #include "vast/CodeGen/CodeGenDriver.hpp"
+#include "vast/CodeGen/CodeGenVisitorList.hpp"
+#include "vast/CodeGen/DefaultVisitor.hpp"
+#include "vast/CodeGen/FallthroughVisitor.hpp"
+#include "vast/CodeGen/TypeCachingProxy.hpp"
+#include "vast/CodeGen/UnsupportedVisitor.hpp"
 #include <clang/AST/ASTContext.h>
+#include <cstdlib>
 #include <iostream>
 #include <memory>
 #include <mlir/IR/DialectRegistry.h>
@@ -24,23 +30,52 @@ int main(int argc, char **argv) {
     return EXIT_FAILURE;
   }
 
-  auto pctx = maybe_ast.TakeValue();
-  auto &actx = pctx.UnderlyingAST();
+  // Load MLIR and VAST dialects into registry.
+  mlir::DialectRegistry registry;
+  mlir::registerAllDialects(registry);
+  vast::registerAllDialects(registry);
+
+  // Insert our custom dialect.
+  registry.insert<macroni::macroni::MacroniDialect>();
+  auto mctx = std::make_unique<vast::mcontext_t>();
+  mctx->appendDialectRegistry(registry);
+  mctx->loadAllAvailableDialects();
+
+  // Set up variables for constructing the codegen driver.
+
+  auto &actx = maybe_ast->UnderlyingAST();
+  auto bld = vast::cg::mk_codegen_builder(*mctx);
+  auto mg = std::make_shared<macroni::pasta_meta_generator>(&actx, &*mctx);
+  auto sg = std::make_shared<vast::cg::default_symbol_generator>(
+      actx.createMangleContext());
+
+  using vast::cg::as_node;
+  using vast::cg::as_node_with_list_ref;
+  auto visitors = std::make_shared<vast::cg::visitor_list>() |
+                  as_node_with_list_ref<macroni::macroni_visitor>(
+                      *mctx, *bld, *mg, maybe_ast.Value()) |
+                  as_node_with_list_ref<vast::cg::attr_visitor_proxy>() |
+                  as_node<vast::cg::type_caching_proxy>() |
+                  as_node_with_list_ref<vast::cg::default_visitor>(
+                      *mctx, *bld, mg, sg,
+                      /* strict return = */ false,
+                      vast::cg::missing_return_policy::emit_trap) |
+                  as_node_with_list_ref<vast::cg::unsup_visitor>(*mctx, *bld) |
+                  as_node<vast::cg::fallthrough_visitor>();
+
+  // Create the codegen driver.
   auto driver =
-      macroni::generate_codegen_driver<macroni::macroni::MacroniDialect,
-                                       macroni::pasta_meta_generator>(actx);
+      std::make_unique<vast::cg::driver>(actx, *mctx, std::move(bld), visitors);
+  driver->enable_verifier(true);
 
-  // Add the macroni visitor first. This ensures that our visitor will try to
-  // lower stmts down to the Macroni dialect first before VAST's dialect.
-  driver->push_visitor(std::make_unique<macroni::macroni_visitor>(
-      pctx, driver->mcontext(), driver->get_codegen_builder(),
-      driver->get_meta_generator(), driver->get_symbol_generator(),
-      vast::cg::visitor_view(driver->get_visitor_stack())));
-
-  vast::cg::setup_default_visitor_stack(*driver);
+  // Emit the lowered MLIR.
 
   driver->emit(actx.getTranslationUnitDecl());
   driver->finalize();
+  if (!driver->verify()) {
+    llvm::errs() << "Failed to verify driver\n";
+    return EXIT_FAILURE;
+  }
   auto mod = driver->freeze();
   mod->print(llvm::outs());
 
