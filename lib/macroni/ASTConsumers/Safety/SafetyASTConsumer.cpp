3dee3f4dcc8389f4941710b3f092f66530f80e37
Brent Pappas <pappasbrent@gmail.com>
2024-07-01 14:19:58 -0400

    Update Safe-C to use updated macroni visitor
    - Update Safe-C to use the updated safety visitor and vast visitor list.
      Inline the logic for doing this for now for ease of understanding; the
      logic for creating the driver and emitting the module will be
      extracted into a function in a later commit.
    - Add error checking by verifying the codegen driver's module.

--- lib/macroni/ASTConsumers/Safety/SafetyASTConsumer.cpp
+++ lib/macroni/ASTConsumers/Safety/SafetyASTConsumer.cpp
@@ -1,9 +1,16 @@
 #include "macroni/ASTConsumers/Safety/SafetyASTConsumer.hpp"
 #include "macroni/ASTMatchers/Safety/SafetyMatchers.hpp"
-#include "macroni/Common/CodeGenDriverSetup.hpp"
 #include "macroni/Common/MacroniMetaGenerator.hpp"
 #include "macroni/Dialect/Safety/SafetyDialect.hpp"
 #include "macroni/Translation/Safety/SafetyVisitor.hpp"
+#include "vast/CodeGen/AttrVisitorProxy.hpp"
+#include "vast/CodeGen/CodeGenDriver.hpp"
+#include "vast/CodeGen/CodeGenVisitorList.hpp"
+#include "vast/CodeGen/DefaultSymbolGenerator.hpp"
+#include "vast/CodeGen/DefaultVisitor.hpp"
+#include "vast/CodeGen/FallthroughVisitor.hpp"
+#include "vast/CodeGen/TypeCachingProxy.hpp"
+#include "vast/CodeGen/UnsupportedVisitor.hpp"
 #include <clang/AST/ASTConsumer.h>
 #include <clang/AST/ASTContext.h>
 #include <clang/ASTMatchers/ASTMatchFinder.h>
@@ -11,31 +18,59 @@
 #include <memory>
 
 namespace macroni::safety {
-void SafetyASTConsumer::HandleTranslationUnit(clang::ASTContext &Ctx) {
+void SafetyASTConsumer::HandleTranslationUnit(clang::ASTContext &actx) {
   // Match safe blocks.
 
   clang::ast_matchers::MatchFinder finder;
   macroni::safety::safe_block_condition_collector matcher;
   finder.addMatcher(macroni::safety::safe_block_condition_matcher, &matcher);
+  finder.matchAST(actx);
+
+  // Load MLIR and VAST dialects into registry.
+  mlir::DialectRegistry registry;
+  mlir::registerAllDialects(registry);
+  vast::registerAllDialects(registry);
+
+  // Insert our custom dialect.
+  registry.insert<SafetyDialect>();
+  auto mctx = std::make_unique<vast::mcontext_t>();
+  mctx->appendDialectRegistry(registry);
+  mctx->loadAllAvailableDialects();
+
+  // Set up variables for constructing the codegen driver.
+
+  auto bld = vast::cg::mk_codegen_builder(*mctx);
+  auto mg = std::make_shared<macroni::macroni_meta_generator>(&actx, &*mctx);
+  auto sg = std::make_shared<vast::cg::default_symbol_generator>(
+      actx.createMangleContext());
+
+  using vast::cg::as_node;
+  using vast::cg::as_node_with_list_ref;
+  auto visitors = std::make_shared<vast::cg::visitor_list>() |
+                  as_node_with_list_ref<safety_visitor>(
+                      *mctx, *bld, matcher.m_safe_block_conditions) |
+                  as_node_with_list_ref<vast::cg::attr_visitor_proxy>() |
+                  as_node<vast::cg::type_caching_proxy>() |
+                  as_node_with_list_ref<vast::cg::default_visitor>(
+                      *mctx, *bld, mg, sg,
+                      /* strict return = */ false,
+                      vast::cg::missing_return_policy::emit_trap) |
+                  as_node_with_list_ref<vast::cg::unsup_visitor>(*mctx, *bld) |
+                  as_node<vast::cg::fallthrough_visitor>();
+
+  // Create the codegen driver.
   auto driver =
-      macroni::generate_codegen_driver<SafetyDialect,
-                                       macroni::macroni_meta_generator>(Ctx);
-  finder.matchAST(Ctx);
-
-  // Generate the driver.
-
-  driver->push_visitor(std::make_unique<safety_visitor>(
-      matcher.m_safe_block_conditions, driver->mcontext(),
-      driver->get_codegen_builder(), driver->get_meta_generator(),
-      driver->get_symbol_generator(),
-      vast::cg::visitor_view(driver->get_visitor_stack())));
-
-  vast::cg::setup_default_visitor_stack(*driver);
+      std::make_unique<vast::cg::driver>(actx, *mctx, std::move(bld), visitors);
+  driver->enable_verifier(true);
 
   // Emit the lowered MLIR.
 
-  driver->emit(Ctx.getTranslationUnitDecl());
+  driver->emit(actx.getTranslationUnitDecl());
   driver->finalize();
+  if (!driver->verify()) {
+    llvm::errs() << "Failed to verify driver\n";
+    return;
+  }
   auto mod = driver->freeze();
   mod->print(llvm::outs());
 }
