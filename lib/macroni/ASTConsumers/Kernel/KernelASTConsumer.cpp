3291689eea4ca05e5099216d2f415620270c5b4f
Brent Pappas <pappasbrent@gmail.com>
2024-07-02 09:37:27 -0400

    Use new generate_module() API

--- lib/macroni/ASTConsumers/Kernel/KernelASTConsumer.cpp
+++ lib/macroni/ASTConsumers/Kernel/KernelASTConsumer.cpp
@@ -6,6 +6,7 @@
 #include "macroni/Conversion/Kernel/KernelRewriters.hpp"
 #include "macroni/Dialect/Kernel/KernelDialect.hpp"
 #include "macroni/Translation/Kernel/KernelVisitor.hpp"
+#include "vast/Frontend/Action.hpp"
 #include <clang/AST/ASTConsumer.h>
 #include <clang/AST/ASTContext.h>
 #include <clang/ASTMatchers/ASTMatchFinder.h>
@@ -32,18 +33,19 @@ void KernelASTConsumer::HandleTranslationUnit(clang::ASTContext &actx) {
   matcher.attach_to(finder);
   finder.matchAST(actx);
 
-  auto mctx = vast::mcontext_t();
-  auto mod =
+  auto maybe_mod_and_context =
       macroni::generate_module<KernelDialect, macroni::macroni_meta_generator,
-                               kernel_visitor>(actx, mctx, actx,
-                                               matcher.expansions);
-  if (!mod) {
+                               kernel_visitor>(actx, actx, matcher.expansions);
+  if (!maybe_mod_and_context) {
     llvm::errs() << "Could not make module\n";
     return;
   }
 
+  auto mod = maybe_mod_and_context->first;
+  auto mctx = std::move(maybe_mod_and_context->second);
+
   // Rewrite rcu_read_lock()/unlock() calls into critical sections.
-  rewrite_rcu(&mctx, mod);
+  rewrite_rcu(&*mctx, mod);
 
   // Handle the result.
   m_module_handler(mod);
